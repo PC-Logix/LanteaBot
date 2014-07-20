0a74228314365087fb859126b1900a73850d5a18
Caitlyn <Michiyo.Ravencroft@gmail.com>
2014-07-20 14:46:26 -0500

    YTInfo now searches for YT links in the middle of a string.

--- src/pcl/lc/irc/hooks/YTInfo.java
+++ src/pcl/lc/irc/hooks/YTInfo.java
@@ -3,8 +3,12 @@
  */
 package pcl.lc.irc.hooks;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
+
 import pcl.lc.utils.getVideoInfo;
 /**
  * @author caitlyn
@@ -20,8 +24,22 @@ public class YTInfo extends ListenerAdapter {
 		String s = ourinput.trim();
 		if (s.length() > 1) {
 			if (event.getChannel().getName() != "#oc") {
-				if (s.indexOf("youtube.com") != -1 || s.indexOf("youtu.be") != -1) {
-					String vinfo = getVideoInfo.getVideoSearch(s, true, false);
+				int matchStart = 1;
+				int matchEnd = 1;
+				final Pattern urlPattern = Pattern.compile(
+				        "(?:^|[\\W])((ht|f)tp(s?):\\/\\/|www\\.)"
+				                + "(([\\w\\-]+\\.){1,}?([\\w\\-.~]+\\/?)*"
+				                + "[\\p{Alnum}.,%_=?&#\\-+()\\[\\]\\*$~@!:/{};']*)",
+				        Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
+				Matcher matcher = urlPattern.matcher(s);
+				while (matcher.find()) {
+				    matchStart = matcher.start(1);
+				    matchEnd = matcher.end();
+				    // now you have the offsets of a URL match
+				}
+				String url = s.substring(matchStart, matchEnd);
+				if (url.indexOf("youtube.com") != -1 || url.indexOf("youtu.be") != -1) {
+					String vinfo = getVideoInfo.getVideoSearch(url, true, false);
 					event.respond(vinfo);
 				}
 			}
