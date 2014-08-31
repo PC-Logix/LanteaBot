c2a9576f1f020c6d565b8429a622a3470e9ca26f
Caitlyn Mainer <Michiyo.Ravencroft@gmail.com>
2014-08-31 11:29:38 -0500

    Calc, and exp4j dep

---
+++ src/main/java/pcl/lc/irc/hooks/Calc.java
@@ -0,0 +1,41 @@
+package pcl.lc.irc.hooks;
+
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+import org.apache.commons.lang3.StringUtils;
+import org.pircbotx.hooks.ListenerAdapter;
+import org.pircbotx.hooks.events.MessageEvent;
+
+import pcl.lc.irc.IRCBot;
+
+/**
+ * @author Caitlyn
+ *
+ */
+@SuppressWarnings("rawtypes")
+public class Calc extends ListenerAdapter {
+	public Calc() {
+		IRCBot.registerCommand("calc");
+	}
+
+	@SuppressWarnings({ "unchecked" })
+	@Override
+	public void onMessage(final MessageEvent event) throws Exception {
+		super.onMessage(event);
+		String prefix = IRCBot.commandprefix;
+		String ourinput = event.getMessage().toLowerCase();
+		String trigger = ourinput.trim();
+		if (trigger.length() > 1) {
+			String[] firstWord = StringUtils.split(trigger);
+			String triggerWord = firstWord[0];
+			if (triggerWord.equals(prefix + "calc")) {
+				String expression = event.getMessage().substring(event.getMessage().indexOf(triggerWord) + triggerWord.length()).trim();
+				Expression e = new ExpressionBuilder(expression).build();
+				double result = e.evaluate();
+				String result2 = Double.toString(result);
+				event.respond(result2);
+			}
+		}
+	}
+}
