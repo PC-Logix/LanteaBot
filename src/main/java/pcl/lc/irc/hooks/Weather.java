917e0292eaebd4dd7fb2b5d23f7aecdbdfd44960
Caitlyn Mainer <Michiyo.Ravencroft@gmail.com>
2014-11-10 15:57:10 -0600

    Moved weather module to new API at worldweatheronline.com

--- src/main/java/pcl/lc/irc/hooks/Weather.java
+++ src/main/java/pcl/lc/irc/hooks/Weather.java
@@ -36,23 +36,34 @@ public class Weather extends ListenerAdapter {
 			String[] firstWord = StringUtils.split(trigger);
 			String triggerWord = firstWord[0];
 			if (triggerWord.equals(prefix + "weather")) {
-				if (IRCBot.botConfig.get("WUndergroundAPI") != null) {
+				if (IRCBot.botConfig.containsKey("WUndergroundAPI")) {
 					String loc = event.getMessage().substring(event.getMessage().indexOf("weather") + 7).trim();
 					DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 					DocumentBuilder db = dbf.newDocumentBuilder();
 					String location = loc;
 					location = URLEncoder.encode(location, "UTF-8").replace("+", "%20");
-					System.out.println("http://api.wunderground.com/api/" + IRCBot.botConfig.get("WUndergroundAPI") + "/conditions/q/" + location + ".xml");
-					Document doc = db.parse(new URL("http://api.wunderground.com/api/" + IRCBot.botConfig.get("WUndergroundAPI") + "/conditions/q/" + location + ".xml").openStream());
+					Document doc = db.parse(new URL("http://api.worldweatheronline.com/free/v2/weather.ashx?q=" + location + "&format=XML&key=15a0c07dbaa418e896c1be9d542b8").openStream());
+					//System.out.println("http://api.wunderground.com/api/" + IRCBot.botConfig.get("WUndergroundAPI") + "/conditions/q/" + location + ".xml");
 					XPathFactory xPathfactory = XPathFactory.newInstance();
 					XPath xpath = xPathfactory.newXPath();
-					String location_name = (String) xpath.evaluate("/response/current_observation/display_location/full", doc, XPathConstants.STRING);
-					String temperature= (String) xpath.evaluate("/response/current_observation/temperature_string", doc, XPathConstants.STRING);
-					String relative_humidity = (String) xpath.evaluate("/response/current_observation/relative_humidity", doc, XPathConstants.STRING);
-					String wind = (String) xpath.evaluate("/response/current_observation/wind_string", doc, XPathConstants.STRING);
-					String weather = (String) xpath.evaluate("/response/current_observation/weather", doc, XPathConstants.STRING);
-					String wind_kph = (String) xpath.evaluate("/response/current_observation/wind_kph", doc, XPathConstants.STRING);
-					event.respond("Current weather for " + location_name + " Current Temp: " + temperature + " Current Humidity: " + relative_humidity + " Wind: " + wind + "/" + wind_kph + " Conditions: " + weather);
+					String location_name = (String) xpath.evaluate("/data/request/query", doc, XPathConstants.STRING);
+					String temp_C = (String) xpath.evaluate("/data/current_condition/temp_C", doc, XPathConstants.STRING);
+					String temp_F = (String) xpath.evaluate("/data/current_condition/temp_F", doc, XPathConstants.STRING);
+					String FeelsLikeC = (String) xpath.evaluate("/data/current_condition/FeelsLikeC", doc, XPathConstants.STRING);
+					String FeelsLikeF = (String) xpath.evaluate("/data/current_condition/FeelsLikeF", doc, XPathConstants.STRING);
+					String humidity = (String) xpath.evaluate("/data/current_condition/humidity", doc, XPathConstants.STRING);
+					String windspeedMiles = (String) xpath.evaluate("/data/current_condition/windspeedMiles", doc, XPathConstants.STRING);
+					String windspeedKmph = (String) xpath.evaluate("/data/current_condition/windspeedKmph", doc, XPathConstants.STRING);
+					String weather = (String) xpath.evaluate("/data/current_condition/weatherDesc", doc, XPathConstants.STRING);
+					String winddir16Point = (String) xpath.evaluate("/data/current_condition/winddir16Point", doc, XPathConstants.STRING);
+
+					if (weather.length() > 0) {
+						event.respond("Current weather for " + location_name + " Current Temp: " + temp_F + "f/" + temp_C + "c Feels Like: " + FeelsLikeF + "f/" + FeelsLikeC + "c Current Humidity: " + humidity + " Wind: From the " + winddir16Point + " " + windspeedMiles + " Mph/" + windspeedKmph + " KPH Conditions: " + weather);					
+					} else {
+						event.getUser().send().notice("No data returned");
+					}
+				} else {
+					event.getUser().send().notice("API Key missing, alert an admin.");
 				}
 			}
 		}
