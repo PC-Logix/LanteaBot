16776dce53c91729fd4fa8f4d72903d49c441aa8
Caitlyn <Michiyo.Ravencroft@gmail.com>
2015-11-02 14:56:29 -0600

    Only trigger if the first character is prefix

--- src/main/java/pcl/lc/irc/hooks/DynamicCommands.java
+++ src/main/java/pcl/lc/irc/hooks/DynamicCommands.java
@@ -35,7 +35,7 @@ public class DynamicCommands extends ListenerAdapter {
 			e.printStackTrace();
 			IRCBot.log.info("An error occurred while processing this command");
 		}
-		
+
 	}
 
 	@SuppressWarnings({ "unchecked" })
@@ -48,54 +48,56 @@ public class DynamicCommands extends ListenerAdapter {
 		String trigger = ourinput.trim();
 		String[] firstWord = StringUtils.split(trigger);
 		String triggerWord = firstWord[0];
+		String firstCharacter = String.valueOf(triggerWord.charAt(0));
 		boolean isOp = IRCBot.getInstance().isOp(event.getBot(), event.getUser());
 		if (trigger.length() > 1) {
 			if (!IRCBot.isIgnored(event.getUser().getNick())) {
-				if (triggerWord.equals(prefix + "addcommand") && (isOp || Helper.isChannelOp(event))) {
-					try {
-						PreparedStatement addCommand = IRCBot.getInstance().getPreparedStatement("addCommand");
-						String[] message = event.getMessage().split(" ", 3);
-						
-						if (!IRCBot.commands.contains(message[1])) {
-							addCommand.setString(1, message[1]);
-							addCommand.setString(2, message[2]);
-							addCommand.executeUpdate();
-							event.respond("Command Added");
-							IRCBot.registerCommand(message[1]);
-						} else {
-							event.respond("Can't override existing commands.");
-						}
-					} catch (Exception e) {
-						e.printStackTrace();
-						event.respond("An error occurred while processing this command");
-					}
-				} else if (triggerWord.equals(prefix + "delcommand") && (isOp || Helper.isChannelOp(event))) {
-					try {
-						PreparedStatement delCommand = IRCBot.getInstance().getPreparedStatement("delCommand");
-						String[] message = event.getMessage().split(" ", 2);
-						delCommand.setString(1, message[1]);
-						delCommand.execute();
-						event.respond("Command deleted");
-						IRCBot.unregisterCommand(message[1]);
-					} catch (Exception e) {
-						e.printStackTrace();
-						event.respond("An error occurred while processing this command");
-					}
-				} else {
-					try {
-						PreparedStatement getCommand = IRCBot.getInstance().getPreparedStatement("getCommand");						
-						getCommand.setString(1, triggerWord.replace(prefix, ""));
-						ResultSet command = getCommand.executeQuery();
-						if(command.next()){
-							event.respond(command.getString(1));
-						}
+				if (firstCharacter == prefix) {
+					if (triggerWord.equals(prefix + "addcommand") && (isOp || Helper.isChannelOp(event))) {
+						try {
+							PreparedStatement addCommand = IRCBot.getInstance().getPreparedStatement("addCommand");
+							String[] message = event.getMessage().split(" ", 3);
 
-					} catch (Exception e) {
-						e.printStackTrace();
-						System.out.println("An error occurred while processing this command");
+							if (!IRCBot.commands.contains(message[1])) {
+								addCommand.setString(1, message[1]);
+								addCommand.setString(2, message[2]);
+								addCommand.executeUpdate();
+								event.respond("Command Added");
+								IRCBot.registerCommand(message[1]);
+							} else {
+								event.respond("Can't override existing commands.");
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+							event.respond("An error occurred while processing this command");
+						}
+					} else if (triggerWord.equals(prefix + "delcommand") && (isOp || Helper.isChannelOp(event))) {
+						try {
+							PreparedStatement delCommand = IRCBot.getInstance().getPreparedStatement("delCommand");
+							String[] message = event.getMessage().split(" ", 2);
+							delCommand.setString(1, message[1]);
+							delCommand.execute();
+							event.respond("Command deleted");
+							IRCBot.unregisterCommand(message[1]);
+						} catch (Exception e) {
+							e.printStackTrace();
+							event.respond("An error occurred while processing this command");
+						}
+					} else {
+						try {
+							PreparedStatement getCommand = IRCBot.getInstance().getPreparedStatement("getCommand");						
+							getCommand.setString(1, triggerWord.replace(prefix, ""));
+							ResultSet command = getCommand.executeQuery();
+							if(command.next()){
+								event.respond(command.getString(1));
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+							System.out.println("An error occurred while processing this command");
+						}
 					}
-				}
-			}		
+				}		
+			}
 		}
 	}
 }
