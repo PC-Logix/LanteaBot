14a0ea07341fe7b33a070d15e39b12211fee2f2d
Caitlyn Mainer <michiyo.ravencroft@gmail.com>
2016-03-10 20:45:11 -0600

    calc moved to abstractlistner

--- src/main/java/pcl/lc/irc/hooks/Calc.java
+++ src/main/java/pcl/lc/irc/hooks/Calc.java
@@ -11,6 +11,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
 
+import pcl.lc.irc.AbstractListener;
 import pcl.lc.irc.Config;
 import pcl.lc.irc.IRCBot;
 
@@ -19,24 +20,25 @@ import pcl.lc.irc.IRCBot;
  *
  */
 @SuppressWarnings("rawtypes")
-public class Calc extends ListenerAdapter {
-	public Calc() {
+public class Calc extends AbstractListener {
+	
+	@Override
+	protected void initCommands() {
 		IRCBot.registerCommand("calc");
 	}
 
-	@SuppressWarnings({ "unchecked" })
 	@Override
-	public void onMessage(final MessageEvent event) throws Exception {
+	public void handleCommand(String sender, final MessageEvent event, String command, String[] args) {
+		String prefix = Config.commandprefix;
 		super.onMessage(event);
 
-		String prefix = Config.commandprefix;
 		String ourinput = event.getMessage().toLowerCase();
 		String trigger = ourinput.trim();
 		String[] splitMessage = event.getMessage().split(" ");
 		if (trigger.length() > 1) {
 			String[] firstWord = StringUtils.split(trigger);
 			String triggerWord = firstWord[0];
-			if (triggerWord.equals(prefix + "calc") || splitMessage[0].startsWith("<") && splitMessage[0].endsWith(">") && splitMessage[1].equals(prefix + "calc")) {
+			if (command.equals(prefix + "calc") || splitMessage[0].startsWith("<") && splitMessage[0].endsWith(">") && splitMessage[1].equals(prefix + "calc")) {
 				String[] message = event.getMessage().split(" ");
 				String expression;
 				if (splitMessage[0].startsWith("<") && splitMessage[0].endsWith(">")) {
