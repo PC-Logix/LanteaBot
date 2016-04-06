d43ef11fd89008290b1678e57edc722f9b33d470
Caitlyn Mainer <michiyo.ravencroft@gmail.com>
2016-04-05 22:27:55 -0500

    Readds Millisecond ping

--- src/main/java/pcl/lc/irc/hooks/Ping.java
+++ src/main/java/pcl/lc/irc/hooks/Ping.java
@@ -15,6 +15,7 @@ import org.pircbotx.hooks.events.NoticeEvent;
 
 import pcl.lc.irc.Config;
 import pcl.lc.irc.IRCBot;
+import pcl.lc.utils.Helper;
 import pcl.lc.utils.TimedHashMap;
 
 /**
@@ -26,9 +27,11 @@ public class Ping extends ListenerAdapter {
 	public Ping() {
 		IRCBot.registerCommand("p", "Sends a CTCP Ping to you, or the user supplied to check latency");
 		IRCBot.registerCommand("ping", "Sends a CTCP Ping to you, or the user supplied to check latency");
+		IRCBot.registerCommand("msp", "Sends a CTCP Ping to you, or the user supplied to check latency, replies with milliseconds");
 	}
 
 	public static TimedHashMap<String, List<Object>> users = new TimedHashMap<String, List<Object>>(60000, null);
+	public static TimedHashMap<String, List<Object>> usersMSP = new TimedHashMap<String, List<Object>>(60000, null);
 
 	@SuppressWarnings({ "unchecked" })
 	@Override
@@ -57,6 +60,23 @@ public class Ping extends ListenerAdapter {
 						IRCBot.bot.sendIRC().ctcpCommand(event.getUser().getNick(), "PING " + System.currentTimeMillis());					
 					}
 				}
+			} else if (triggerWord.equals(prefix + "msp") || triggerWord.equals(prefix + "msping")) {
+				if (!IRCBot.isIgnored(event.getUser().getNick())) {
+					List<Object> eventData = new ArrayList<Object>();
+					eventData.add(event.getChannel().getName());
+					eventData.add(System.currentTimeMillis());
+					if (message.length > 1) {
+						for (User u : event.getChannel().getUsers()) {
+							if(u.getNick().toLowerCase().equals(message[1])) {
+								usersMSP.put(message[1].toLowerCase(), eventData);
+								IRCBot.bot.sendIRC().ctcpCommand(message[1].toLowerCase(), "PING " + System.currentTimeMillis());	
+							}
+						}
+					} else {
+						usersMSP.put(event.getUser().getNick().toLowerCase(), eventData);
+						IRCBot.bot.sendIRC().ctcpCommand(event.getUser().getNick(), "PING " + System.currentTimeMillis());					
+					}
+				}
 			}			
 		}
 	}
@@ -72,8 +92,18 @@ public class Ping extends ListenerAdapter {
 
 				DecimalFormat df = new DecimalFormat("#.##");
 
-				IRCBot.bot.sendIRC().message(channel, "Ping reply from " + event.getUser().getNick() + " " + df.format(time / 1000) + "s");
+				IRCBot.bot.sendIRC().message(channel, "Ping reply from " + Helper.antiPing(event.getUser().getNick()) + " " + df.format(time / 1000) + "s");
 				users.remove(event.getUser().getNick().toLowerCase());
+			} else if (usersMSP.containsKey(event.getUser().getNick().toLowerCase())) {
+				long currentTime = System.currentTimeMillis();
+				String channel = (String) usersMSP.get(event.getUser().getNick().toLowerCase()).get(0);
+				Long timeStamp = (Long) usersMSP.get(event.getUser().getNick().toLowerCase()).get(1);
+				float time = currentTime - timeStamp;
+
+				DecimalFormat df = new DecimalFormat("#.##");
+
+				IRCBot.bot.sendIRC().message(channel, "Ping reply from " + Helper.antiPing(event.getUser().getNick()) + " " + time + "ms");
+				usersMSP.remove(event.getUser().getNick().toLowerCase());
 			}
 		}
 	}
