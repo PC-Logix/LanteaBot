b71dfd629c3157f101553a379b5693e88f84df55
Caitlyn <Michiyo.Ravencroft@gmail.com>
2016-07-12 10:15:00 -0500

    Because

--- src/main/java/pcl/lc/utils/Helper.java
+++ src/main/java/pcl/lc/utils/Helper.java
@@ -47,7 +47,10 @@ public class Helper {
 	}
 	
     private static String addZeroWidthSpace(String s) {
-        return s.replaceAll(".(?=.)", "$0" + "\u200B");
+    	final int mid = s1.length() / 2; //get the middle of the String
+	String[] parts = {s1.substring(0, mid),s1.substring(mid)};
+	return parts[0] + "\u200B" + parts[1];
+        //return s.replaceAll(".(?=.)", "$0" + "\u200B");
     }
 	
 	public static String antiPing(String nick) {
