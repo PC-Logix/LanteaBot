0b77f2f80b32d2c44bf60482c8f4e871dc82e98a
Caitlyn Mainer <michiyo.ravencroft@gmail.com>
2016-08-26 12:54:28 -0500

    Change search command

--- src/main/java/pcl/lc/irc/hooks/Search.java
+++ src/main/java/pcl/lc/irc/hooks/Search.java
@@ -1,4 +1,5 @@
 package pcl.lc.irc.hooks;
+import org.json.JSONException;
 import org.pircbotx.Colors;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
@@ -8,6 +9,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -18,6 +20,8 @@ import org.apache.commons.lang3.StringUtils;
 
 import pcl.lc.irc.Config;
 import pcl.lc.irc.IRCBot;
+import pcl.lc.utils.GoogleSearch;
+import pcl.lc.utils.SearchResult;
 @SuppressWarnings("rawtypes")
 public class Search extends ListenerAdapter {
 
@@ -54,36 +58,26 @@ public class Search extends ListenerAdapter {
 			doSearch = true;
 		}
 		if (doSearch)
-			event.respond(performSearch(filter, StringUtils.join(splitMessage, " ", 1, splitMessage.length)));
+			try {
+				event.respond(performSearch(filter, StringUtils.join(splitMessage, " ", 1, splitMessage.length)).get(0).getSuggestedReturn());
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	}
 
-	private String performSearch(String filter, String terms) {
-		try {
-			StringBuilder searchURLString = new StringBuilder();
-			searchURLString.append("https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=");
-			if (filter != null) {
-				searchURLString.append(filter).append("+");
-			}
-			searchURLString.append(terms.replace(" ", "+"));
-			URL searchURL = new URL(searchURLString.toString());
-			URLConnection conn = searchURL.openConnection();
-			conn.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 RavenBot/2.0");
-			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			StringBuilder json = new StringBuilder();
-			String line;
-			while ((line = in.readLine()) != null) {
-				json.append(line).append("\n");
-			}
-			in.close();
-			JsonElement element = new JsonParser().parse(json.toString());
-			JsonObject output = element.getAsJsonObject().getAsJsonObject("responseData").getAsJsonArray("results").get(0).getAsJsonObject();
-			String title = StringEscapeUtils.unescapeJava(StringEscapeUtils.unescapeHtml4(output.get("titleNoFormatting").toString().replaceAll("\"", "")));
-			String content = StringEscapeUtils.unescapeJava(StringEscapeUtils.unescapeHtml4(output.get("content").toString().replaceAll("\\s+", " ").replaceAll("\\<.*?>", "").replaceAll("\"", "")));
-			String url = StringEscapeUtils.unescapeJava(output.get("unescapedUrl").toString().replaceAll("\"", ""));
-			return url + " - " + Colors.BOLD + title + Colors.NORMAL + ": \"" + content + "\"";
-		} catch (IOException e) {
-			e.printStackTrace();
+	private List<SearchResult> performSearch(String filter, String terms) throws JSONException {
+		StringBuilder searchURLString = new StringBuilder();
+		//searchURLString.append("https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=");
+		if (filter != null) {
+			searchURLString.append(filter).append("+");
 		}
-		return null;
+		searchURLString.append(terms.replace(" ", "+"));
+		List<SearchResult> results = GoogleSearch.performSearch(
+				"018291224751151548851%3Ajzifriqvl1o",
+				searchURLString.toString());
+
+		//return url + " - " + Colors.BOLD + title + Colors.NORMAL + ": \"" + content + "\"";
+		return results;
 	}
 }
---
+++ src/main/java/pcl/lc/utils/GoogleSearch.java
@@ -0,0 +1,116 @@
+/**
+ *     Copyright 2015-2016 Austin Keener
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package pcl.lc.utils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class GoogleSearch
+{
+    public static final String GOOGLE_URL = "https://www.googleapis.com/customsearch/v1?cx=%s&key=%s&num=%d&q=%s";
+    private static String GOOGLE_API_KEY = null;
+    private static LocalDateTime dayStartTime = null;
+    private static int currentGoogleUsage = 0;
+
+    public static void setup(String googleApiKey)
+    {
+        GOOGLE_API_KEY = googleApiKey;
+        dayStartTime = LocalDateTime.now();
+    }
+
+    public static List<SearchResult> performSearch(String engineId, String terms) throws JSONException
+    {
+        return performSearch(engineId, terms, 1);
+    }
+
+    public static List<SearchResult> performSearch(String engineId, String terms, int requiredResultsCount) throws JSONException
+    {
+        try
+        {
+            if (GOOGLE_API_KEY == null)
+                throw new IllegalStateException("Google API Key is null, Cannot preform google search without a key! Set one in the settings!");
+            if (engineId == null || engineId.isEmpty())
+                throw new IllegalArgumentException("Google Custom Search Engine id cannot be null or empty!");
+
+            LocalDateTime currentTime = LocalDateTime.now();
+            if (currentTime.isAfter(dayStartTime.plusDays(1)))
+            {
+                dayStartTime = currentTime;
+                currentGoogleUsage = 1;
+            }
+            else if (currentGoogleUsage >= 80)
+            {
+                throw new IllegalStateException("Google usage has reached the premature security cap of 80");
+            }
+
+            terms = terms.replace(" ", "%20");
+            String searchUrl = String.format(GOOGLE_URL, engineId, GOOGLE_API_KEY, requiredResultsCount, terms);
+
+            URL searchURL = new URL(searchUrl);
+            URLConnection conn = searchURL.openConnection();
+            currentGoogleUsage++;
+            conn.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 " + randomName(10));
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder json = new StringBuilder();
+            String line;
+            while ((line = in.readLine()) != null) {
+                json.append(line).append("\n");
+            }
+            in.close();
+
+            JSONArray jsonResults = new JSONObject(json.toString()).getJSONArray("items");
+            List<SearchResult> results = new LinkedList<>();
+            for (int i = 0; i < jsonResults.length(); i++)
+            {
+                results.add(SearchResult.fromGoogle(jsonResults.getJSONObject(i)));
+            }
+            return results;
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String randomName(int randomLength)
+    {
+        char[] characters = new char[]
+                {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+                'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                '1','2','3','4','5','6','7','8','9','0'};
+
+        Random rand = new Random();
+        StringBuilder builder = new StringBuilder();
+        builder.append("MichiBot/");
+        for (int i = 0; i < randomLength; i++)
+        {
+            builder.append(characters[rand.nextInt(characters.length)]);
+        }
+        return builder.toString();
+    }
+}
