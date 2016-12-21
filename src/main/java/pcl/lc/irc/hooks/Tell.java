37016fdc9584163235a4c17a705b949ff878bf62
Caitlyn Mainer <Michiyo.Ravencroft@gmail.com>
2016-12-21 14:51:47 -0600

    Tells can now be sent from query, and now have a timestamp

--- src/main/java/pcl/lc/irc/hooks/Tell.java
+++ src/main/java/pcl/lc/irc/hooks/Tell.java
@@ -1,7 +1,11 @@
 package pcl.lc.irc.hooks;
 
+import java.util.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 import org.apache.commons.lang3.StringUtils;
 import org.pircbotx.PircBotX;
@@ -9,20 +13,21 @@ import org.pircbotx.User;
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.JoinEvent;
 import org.pircbotx.hooks.events.MessageEvent;
+import org.pircbotx.hooks.types.GenericMessageEvent;
 
+import pcl.lc.irc.AbstractListener;
 import pcl.lc.irc.Config;
 import pcl.lc.irc.IRCBot;
 
 //Author: smbarbour
 
 @SuppressWarnings("rawtypes")
-public class Tell extends ListenerAdapter {
+public class Tell extends AbstractListener {
 
+	public String dest;
 
-	public Tell() {
-		IRCBot.registerCommand("tell", "Sends a tell to the supplied user, with the supplied message " + Config.commandprefix + "tell Michiyo Hello!");
-	}
-
+	public String chan;
+	
 	@Override
 	public void onJoin(final JoinEvent event) {
 		int numTells = 0;
@@ -32,7 +37,6 @@ public class Tell extends ListenerAdapter {
             ResultSet results = checkTells.executeQuery();
             while (results.next()) {
             	numTells++;
-                //bot.sendIRC().notice(sender.getNick(), results.getString(2) + " in " + results.getString(3) + " said: " + results.getString(4));
             }
             if (numTells > 0)
             	event.getUser().send().notice("You have " + numTells + " tell(s) currently waiting for you.");
@@ -40,15 +44,39 @@ public class Tell extends ListenerAdapter {
             e.printStackTrace();
         }
 	}
-	
+
 	@Override
-    public void onMessage(final MessageEvent event) {
+	protected void initCommands() {
+		IRCBot.registerCommand("tell", "Sends a tell to the supplied user, with the supplied message " + Config.commandprefix + "tell Michiyo Hello!");
+	}
+
+	@Override
+	public void handleCommand(String sender, MessageEvent event, String command, String[] args) {
+		if (command.equals(Config.commandprefix + "tell")) {
+			chan = event.getChannel().getName();
+		}
+	}
+
+	@Override
+	public void handleCommand(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {
         PircBotX bot = event.getBot();
-        User sender = event.getUser();
+        String sender = nick;
         if (event.getMessage().startsWith(Config.commandprefix + "tell")) {
-            try {
+			if (event.getClass().getName().equals("org.pircbotx.hooks.events.MessageEvent")) {
+				dest = chan;
+			} else {
+				dest = "query";
+			}
+			String message = "";
+        	try {
+        		
+    			for( int i = 0; i < copyOfRange.length; i++)
+    			{
+    				message = message + " " + copyOfRange[i];
+    			}
+        		
                 PreparedStatement addTell = IRCBot.getInstance().getPreparedStatement("addTell");
-                String[] splitMessage = event.getMessage().split(" ");
+                String[] splitMessage = message.split(" ");
                 if (splitMessage.length == 1) {
                     event.respond("Who did you want to tell?");
                     return;
@@ -59,13 +87,15 @@ public class Tell extends ListenerAdapter {
                     return;
                 }
 
-                String channel = event.getChannel().getName();
-                String message = StringUtils.join(splitMessage," ", 2, splitMessage.length);
-
-                addTell.setString(1, sender.getNick());
+                String channel = dest;
+                SimpleDateFormat f = new SimpleDateFormat("MMM dd @ HH:mm");
+                f.setTimeZone(TimeZone.getTimeZone("UTC"));
+                System.out.println(f.format(new Date()));
+                String messageOut = StringUtils.join(splitMessage," ", 2, splitMessage.length) + " on " + f.format(new Date()) + " UTC";
+                addTell.setString(1, sender);
                 addTell.setString(2, recipient);
                 addTell.setString(3, channel);
-                addTell.setString(4, message);
+                addTell.setString(4, messageOut);
                 addTell.executeUpdate();
                 event.respond(recipient + " will be notified of this message when next seen.");
             } catch (Exception e) {
@@ -73,15 +103,18 @@ public class Tell extends ListenerAdapter {
                 event.respond("An error occurred while processing this command (" + Config.commandprefix + "tell)");
             }
         }
+
+	}
+    public void onMessage(final MessageEvent event) {
         try {
             PreparedStatement checkTells = IRCBot.getInstance().getPreparedStatement("getTells");
-            checkTells.setString(1, sender.getNick());
+            checkTells.setString(1, event.getUser().getNick());
             ResultSet results = checkTells.executeQuery();
             while (results.next()) {
-                bot.sendIRC().notice(sender.getNick(), results.getString(2) + " in " + results.getString(3) + " said: " + results.getString(4));
+                event.getBot().sendIRC().notice(event.getUser().getNick(), results.getString(2) + " in " + results.getString(3) + " said: " + results.getString(4));
             }
             PreparedStatement clearTells = IRCBot.getInstance().getPreparedStatement("removeTells");
-            clearTells.setString(1, sender.getNick());
+            clearTells.setString(1, event.getUser().getNick());
             clearTells.execute();
         } catch (Exception e) {
             e.printStackTrace();
