9bcbae1d14c1e7f3a55b681d12994325961ad13f
Forecaster <martinek90@hotmail.com>
2017-03-02 23:33:19 +0100

    Addendum to sub-command (See next commit for usage for real this time)

--- src/main/java/pcl/lc/irc/IRCBot.java
+++ src/main/java/pcl/lc/irc/IRCBot.java
@@ -96,7 +96,7 @@ public class IRCBot {
 	 */
 	public static void registerCommand(String command, String help, Integer rateLimit) {
 		if (!commands.containsKey(command)) {
-			commands.put(command, new Command(command, Thread.currentThread().getStackTrace()[2].getClassName(), rateLimit));
+			commands.put(command, new Command(command, Thread.currentThread().getStackTrace()[2].getClassName(), rateLimit, false));
 			helpList.put(command, help);	
 			log.info("Registering Command: " + command);
 		} else {
