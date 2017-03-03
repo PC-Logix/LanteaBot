7f893cd00f38f4ec3686598834c4f98c4648c321
Forecaster <martinek90@hotmail.com>
2017-03-03 10:48:15 +0100

    Update LookUp

--- src/main/java/pcl/lc/irc/hooks/LookUp.java
+++ src/main/java/pcl/lc/irc/hooks/LookUp.java
@@ -7,6 +7,7 @@ import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.types.GenericMessageEvent;
 
 import pcl.lc.irc.AbstractListener;
+import pcl.lc.irc.Command;
 import pcl.lc.irc.Config;
 import pcl.lc.irc.IRCBot;
 
@@ -16,21 +17,35 @@ import pcl.lc.irc.IRCBot;
  */
 @SuppressWarnings("rawtypes")
 public class LookUp extends AbstractListener {
+	private Command local_command_lookup;
+	private Command local_command_rdns;
 
 	@Override
 	protected void initHook() {
-		IRCBot.registerCommand("lookup", "Returns DNS information");
+		local_command_lookup = new Command("lookup", 0);
+		local_command_rdns = new Command("rdns", 0);
+		IRCBot.registerCommand(local_command_lookup, "Returns DNS information");
+		IRCBot.registerCommand(local_command_rdns, "Returns Reverse DNS information");
 	}
 
+	public String chan;
+	public String target = null;
 	@Override
 	public void handleCommand(String sender, MessageEvent event, String command, String[] args) {
-		// TODO Auto-generated method stub
+		if (local_command_lookup.shouldExecuteBool(command)) {
+			chan = event.getChannel().getName();
+		}
 		
 	}
 
 	@Override
 	public void handleCommand(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {
-		if (command.equals(Config.commandprefix + "lookup")) {
+		if (!event.getClass().getName().equals("org.pircbotx.hooks.events.MessageEvent")) {
+			target = nick;
+		} else {
+			target = chan;
+		}
+		if (local_command_lookup.shouldExecuteBool(command, nick)) {
 			if (!IRCBot.isIgnored(event.getUser().getNick())) {
 				InetAddress[] inetAddressArray = null;
 				try {
@@ -45,7 +60,7 @@ public class LookUp extends AbstractListener {
 				}
 				event.respond(output.replace(copyOfRange[0] + "/", " ").replaceAll("((?::0\\b){2,}):?(?!\\S*\\b\\1:0\\b)(\\S*)", "::$2"));
 			}
-		} else if (command.equals(Config.commandprefix + "rdns")) {
+		} else if (local_command_rdns.shouldExecuteBool(command, nick)) {
 			if (!IRCBot.isIgnored(event.getUser().getNick())) {
 				InetAddress addr = null;
 				try {
