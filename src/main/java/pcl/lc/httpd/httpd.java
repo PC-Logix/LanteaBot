ddcf6755e75ce31c71056e7455e99b900767a16f
Caitlyn Mainer <Michiyo.Ravencroft@gmail.com>
2017-03-22 14:05:57 -0500

    add default index handler, and index.html

--- src/main/java/pcl/lc/httpd/httpd.java
+++ src/main/java/pcl/lc/httpd/httpd.java
@@ -1,10 +1,29 @@
 package pcl.lc.httpd;
 
+import static org.apache.commons.lang3.StringEscapeUtils.escapeHtml4;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import pcl.lc.irc.Config;
+import pcl.lc.irc.Database;
 import pcl.lc.irc.IRCBot;
 
 @SuppressWarnings("restriction")
@@ -19,13 +38,14 @@ public class httpd {
      * @param route
      * @param handlerIn
      */
-	public void registerContext(String route,  HttpHandler handlerIn) {
+	public static void registerContext(String route,  HttpHandler handlerIn) {
 		if(server != null)
 			server.createContext(route, handlerIn);
     }
     
 	public static void start() throws Exception {
 		if(server != null) {
+			registerContext("/", new IndexHandler());
 			IRCBot.log.info("Starting HTTPD On port " + Config.httpdport + " Base domain: " + Config.httpdBaseDomain);
 			server.setExecutor(null); // creates a default executor
 	        server.start();
@@ -39,5 +59,31 @@ public class httpd {
 	
 	public static String getBaseDomain() {
 		return baseDomain;
+	}
+	
+	static class IndexHandler implements HttpHandler {
+		
+		static String html;
+		@Override
+		public void handle(HttpExchange t) throws IOException {
+			InputStream htmlIn = getClass().getResourceAsStream("/html/index.html");
+			html = CharStreams.toString(new InputStreamReader(htmlIn, Charsets.UTF_8));
+
+			String target = t.getRequestURI().toString();
+			String response = "";
+
+			// convert String into InputStream
+			InputStream is = new ByteArrayInputStream(html.getBytes());
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+				String line = null;
+				while ((line = br.readLine()) != null) {
+					response = response + line.replace("#BODY#", target).replace("#BOTNICK#", IRCBot.getOurNick())+"\n";
+				}
+			}
+			t.sendResponseHeaders(200, response.getBytes().length);
+			OutputStream os = t.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		}
 	}
 }
