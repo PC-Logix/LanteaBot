18416e33e5eff1813e46711c5dbda77427ca8cfc
Forecaster <martinek90@hotmail.com>
2017-03-25 16:25:25 +0100

    Migrate Juggle

--- src/main/java/pcl/lc/irc/hooks/Juggle.java
+++ src/main/java/pcl/lc/irc/hooks/Juggle.java
@@ -8,6 +8,8 @@ import org.pircbotx.hooks.types.GenericMessageEvent;
 import pcl.lc.irc.*;
 import pcl.lc.utils.Helper;
 
+import java.util.ArrayList;
+
 /**
  * @author Caitlyn
  *
@@ -19,7 +21,47 @@ public class Juggle extends AbstractListener {
 
 	@Override
 	protected void initHook() {
-		local_command = new Command("juggle", 0);
+		local_command = new Command("juggle", 0) {
+			@Override
+			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, ArrayList<String> params) {
+				local_command.updateLastExecution();
+				int item_amount = 3;
+				try {
+					item_amount = Integer.parseInt(params.get(0));
+				} catch (Exception ex) {
+					ex.printStackTrace();
+					System.out.println("No valid amount specified. Defaulting to " + item_amount);
+				}
+				item_amount = Math.min(6, item_amount);
+				item_amount = Math.max(1, item_amount);
+
+				ItemCollection items = new ItemCollection();
+				if (!items.fillWithUniqueItems(item_amount)) {
+					Helper.sendMessage(target, "I can't find any items to juggle with.", nick);
+					return;
+				}
+
+				Helper.sendAction(target, "juggles with " + items.getItemNames());
+				int dropped = 0;
+				for (Item item : items.getItems()) {
+					if (item != null) {
+						int drop_roll = Helper.getRandomInt(0, 100);
+						if (drop_roll < (100 * (base_drop_chance + (0.10 * item_amount)))) {
+							dropped++;
+							int damage = Helper.getRandomInt(1, 3);
+							String dust = item.damage(damage);
+							Helper.sendAction(target, "drops " + item.getName() + " which takes " + damage + " damage" + dust);
+						}
+					}
+				}
+				if (dropped > 0)
+					Helper.sendMessage(target, Helper.get_fail_response());
+				else {
+					Helper.sendAction(target, "doesn't drop anything");
+					Helper.sendMessage(target, Helper.get_success_response());
+				}
+			}
+		};
 		IRCBot.registerCommand(local_command, "Juggle with items");
 	}
 
@@ -27,62 +69,17 @@ public class Juggle extends AbstractListener {
 	public String target = null;
 	@Override
 	public void handleCommand(String sender, MessageEvent event, String command, String[] args) {
-		if (local_command.shouldExecuteBool(command, event)) {
-			chan = event.getChannel().getName();
-		}
+		chan = event.getChannel().getName();
 	}
 
 	@Override
 	public void handleCommand(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {
-		long shouldExecute = local_command.shouldExecute(command, event);
 		if (!event.getClass().getName().equals("org.pircbotx.hooks.events.MessageEvent")) {
 			target = nick;
 		} else {
 			target = chan;
 		}
-		if (shouldExecute == 0) {
-			local_command.updateLastExecution();
-			int item_amount = 3;
-			try {
-				item_amount = Integer.parseInt(copyOfRange[0]);
-			} catch (Exception ex) {
-				ex.printStackTrace();
-				System.out.println("No valid amount specified. Defaulting to " + item_amount);
-			}
-			item_amount = Math.min(6, item_amount);
-			item_amount = Math.max(1, item_amount);
-
-			ItemCollection items = new ItemCollection();
-			if (!items.fillWithUniqueItems(item_amount)) {
-				Helper.sendMessage(target, "I can't find any items to juggle with.", nick);
-				return;
-			}
-
-			Helper.sendAction(target, "juggles with " + items.getItemNames());
-			int dropped = 0;
-			for (Item item : items.getItems()) {
-				if (item != null) {
-					int drop_roll = Helper.getRandomInt(0, 100);
-					if (drop_roll < (100 * (base_drop_chance + (0.10 * item_amount)))) {
-						dropped++;
-						int damage = Helper.getRandomInt(1, 3);
-						String dust = item.damage(damage);
-						Helper.sendAction(target, "drops " + item.getName() + " which takes " + damage + " damage" + dust);
-					}
-				}
-			}
-			if (dropped > 0)
-				Helper.sendMessage(target, Helper.get_fail_response());
-			else {
-				Helper.sendAction(target, "doesn't drop anything");
-				Helper.sendMessage(target, Helper.get_success_response());
-			}
-		}
-		else {
-			String reason = local_command.getCannotExecuteReason(shouldExecute);
-			if ( reason != null && reason != "")
-				Helper.sendMessage(target , reason, nick);
-		}
+		local_command.tryExecute(command, nick, target, event, copyOfRange);
 	}
 
 	@Override
