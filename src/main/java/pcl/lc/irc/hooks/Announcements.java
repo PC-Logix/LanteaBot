5c0cc4bc0152842c31c7edadd8534941fc25818f
Forecaster <martinek90@hotmail.com>
2017-03-25 10:37:10 +0100

    Migrate Announcements

--- src/main/java/pcl/lc/irc/hooks/Announcements.java
+++ src/main/java/pcl/lc/irc/hooks/Announcements.java
@@ -19,8 +19,10 @@ import org.pircbotx.hooks.events.MessageEvent;
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
+import org.pircbotx.hooks.types.GenericMessageEvent;
 import pcl.lc.irc.*;
 import pcl.lc.utils.CommentedProperties;
+import pcl.lc.utils.Helper;
 
 
 /**
@@ -28,7 +30,7 @@ import pcl.lc.utils.CommentedProperties;
  *
  */
 @SuppressWarnings("rawtypes")
-public class Announcements extends ListenerAdapter implements Runnable {
+public class Announcements extends AbstractListener {
 	private Command local_command_add;
 	private Command local_command_list;
 	private Command local_command_remove;
@@ -36,53 +38,100 @@ public class Announcements extends ListenerAdapter implements Runnable {
 
 	public static Builder config = new Configuration.Builder();
 	public static CommentedProperties prop = new CommentedProperties();
-	public static HashMap<String, List<Object>> Announcements = new HashMap<String, List<Object>>();
-	private Boolean chanOp = false;
-	private int permLevel = 0;
-	private int requiredPermLevel = 5;
-	XStream xstream = new XStream(new DomDriver());
-	
-	public static void saveProps() {
-		FileOutputStream output = null;
-		try {
-			output = new FileOutputStream("announcements.xml");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		try {
-			prop.store(output, null);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public static HashMap<String, List<Object>> Announcements = new HashMap<>();
+
+	@Override
+	protected void initHook() {
+		initCommands();
+		IRCBot.registerCommand(local_command_add);
+		IRCBot.registerCommand(local_command_list);
+		IRCBot.registerCommand(local_command_remove);
+		IRCBot.registerCommand(local_command_reload);
+		Database.addStatement("CREATE TABLE IF NOT EXISTS Announcements(channel, schedule, title, message)");
+		Database.addPreparedStatement("addAnnounce", "INSERT INTO Announcements(channel, schedule, message) VALUES (?,?,?);");
+		Database.addPreparedStatement("getAnnounce", "SELECT schedule, title, message FROM Announcements WHERE channel = ?;");
+		Database.addPreparedStatement("delAnnounce", "DELETE FROM Announcements WHERE title = ? AND channel = ?;");
+		setConfig();
 	}
 
-	public static void setConfig() {
+	private void initCommands() {
+		local_command_add = new Command("addannounce", 0, Permissions.ADMIN) {
+			@Override
+			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, String params) {
+				Helper.sendMessage(target, "This command doesn't do anything.", nick);
+			}
+		}; local_command_add.setHelpText("Add announce message");
+		local_command_list = new Command("listannounce", 0, Permissions.ADMIN) {
+			@Override
+			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, String params) {
+				Helper.sendMessage(target, "This command doesn't do anything.", nick);
+			}
+		}; local_command_list.setHelpText("List announce messages");
+		local_command_remove = new Command("removeannounce", 0, Permissions.ADMIN) {
+			@Override
+			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, String params) {
+				Helper.sendMessage(target, "This command doesn't do anything.", nick);
+			}
+		}; local_command_remove.setHelpText("Remove announce message");
+		local_command_reload = new Command("reloadannounce", 0, Permissions.ADMIN) {
+			@Override
+			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, String params) {
+				Helper.sendMessage(target, "This command doesn't do anything.", nick);
+			}
+		}; local_command_reload.setHelpText("Reload announce messages");
+	}
+
+	public String chan;
+	public String target = null;
+	@Override
+	public void handleCommand(String sender, MessageEvent event, String command, String[] args) {
+		chan = event.getChannel().getName();
+	}
+
+	@Override
+	public void handleCommand(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {
+		if (!event.getClass().getName().equals("org.pircbotx.hooks.events.MessageEvent")) {
+			target = nick;
+		} else {
+			target = chan;
+		}
+		local_command_add.tryExecute(command, nick, target, event, copyOfRange);
+		local_command_list.tryExecute(command, nick, target, event, copyOfRange);
+		local_command_remove.tryExecute(command, nick, target, event, copyOfRange);
+		local_command_reload.tryExecute(command, nick, target, event, copyOfRange);
+	}
+
+	@Override
+	public void handleMessage(String sender, MessageEvent event, String command, String[] args) {}
+
+	@Override
+	public void handleMessage(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {}
+
+	private static void setConfig() {
 		InputStream input = null;
 
 		try {
-			
-	        File file = new File("announcements.xml");
-	        if (!file.exists()) {
-	        	System.out.println("Creating announcements.xml");
-	        	file.createNewFile();
-	        }
-			
+
+			File file = new File("announcements.xml");
+			if (!file.exists()) {
+				System.out.println("Creating announcements.xml");
+				file.createNewFile();
+			}
+
 			input = new FileInputStream(file);
 			// load a properties file
 			prop.load(input);
 			Announcements.clear();
 			for(String key : prop.stringPropertyNames()) {
-				  List<Object> eventData = new ArrayList<Object>();
-				  eventData.add("Channel");
-				  eventData.add("Event");
-				  eventData.add("Message");
-				  Announcements.put(key, eventData);
-				}
+				List<Object> eventData = new ArrayList<Object>();
+				eventData.add("Channel");
+				eventData.add("Event");
+				eventData.add("Message");
+				Announcements.put(key, eventData);
+			}
 			IRCBot.log.info(Announcements.toString());
 			System.out.println(Announcements.toString());
-			
+
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		} finally {
@@ -94,50 +143,5 @@ public class Announcements extends ListenerAdapter implements Runnable {
 				}
 			}
 		}
-	}
-	
-	public Announcements() {
-		local_command_add = new Command("addannounce", 0);
-		IRCBot.registerCommand(local_command_add, "Add announce message");
-		local_command_list = new Command("listannounce", 0);
-		IRCBot.registerCommand(local_command_list, "List announce messages");
-		local_command_remove = new Command("removeannounce", 0);
-		IRCBot.registerCommand(local_command_remove, "Remove announce message");
-		local_command_reload = new Command("reloadannounce", 0);
-		IRCBot.registerCommand(local_command_reload, "Reload announce messages");
-		Database.addStatement("CREATE TABLE IF NOT EXISTS Announcements(channel, schedule, title, message)");
-		Database.addPreparedStatement("addAnnounce", "INSERT INTO Announcements(channel, schedule, message) VALUES (?,?,?);");
-		Database.addPreparedStatement("getAnnounce", "SELECT schedule, title, message FROM Announcements WHERE channel = ?;");
-		Database.addPreparedStatement("delAnnounce", "DELETE FROM Announcements WHERE title = ? AND channel = ?;");
-		setConfig();
-	}
-
-	@SuppressWarnings({ "unchecked" })
-	@Override
-	public void onMessage(final MessageEvent event) throws Exception {
-		super.onMessage(event);
-		if (Permissions.hasPermission(event.getBot(), event, requiredPermLevel)) {
-			String ourInput = event.getMessage().toLowerCase();
-			String trigger = ourInput.trim();
-			if (trigger.length() > 1) {
-				String[] firstWord = StringUtils.split(trigger);
-				String command = firstWord[0];
-				if (local_command_add.shouldExecute(command, event) == 0) {
-					event.respond("Merp");
-				} else if (local_command_list.shouldExecute(command, event) == 0) {
-					
-				} else if (local_command_remove.shouldExecute(command, event) == 0) {
-					
-				} else if (local_command_reload.shouldExecute(command, event) == 0) {
-					setConfig();
-				}
-			}
-		}
-	}
-
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
 	}
 }
