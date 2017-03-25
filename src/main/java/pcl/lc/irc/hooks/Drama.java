4c8dcfd152cd5749ee7acca3bebaebd47feb87da
Forecaster <martinek90@hotmail.com>
2017-03-25 17:42:01 +0100

    Migrate Drama

--- src/main/java/pcl/lc/irc/hooks/Drama.java
+++ src/main/java/pcl/lc/irc/hooks/Drama.java
@@ -1,7 +1,14 @@
 /**
- * 
+ *
  */
 package pcl.lc.irc.hooks;
+
+import org.pircbotx.hooks.events.MessageEvent;
+import org.pircbotx.hooks.types.GenericMessageEvent;
+import pcl.lc.irc.AbstractListener;
+import pcl.lc.irc.Command;
+import pcl.lc.irc.IRCBot;
+import pcl.lc.utils.Helper;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,43 +17,26 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.pircbotx.hooks.events.MessageEvent;
-import org.pircbotx.hooks.types.GenericMessageEvent;
-
-import pcl.lc.irc.AbstractListener;
-import pcl.lc.irc.Command;
-import pcl.lc.irc.Config;
-import pcl.lc.irc.IRCBot;
-import pcl.lc.utils.Helper;
-
 /**
  * @author Caitlyn
- *
  */
 @SuppressWarnings("rawtypes")
 public class Drama extends AbstractListener {
 	private Command local_command;
+
 	@Override
 	protected void initHook() {
-		local_command = new Command("drama", 0);
-		IRCBot.registerCommand(local_command, "Generates random drama using Mod Developers, Projects, and other semi random data.");
-	}
-
-
-	@Override
-	public void handleCommand(String sender, MessageEvent event, String command, String[] args) {
-		String ourinput = event.getMessage().toLowerCase();
-		String trigger = ourinput.trim();
-		if (trigger.length() > 1) {
-			long shouldExecute = local_command.shouldExecute(command, event);
-			if (shouldExecute == 0) {
+		local_command = new Command("drama", 0) {
+			@Override
+			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, String params) {
 				if (!IRCBot.isIgnored(event.getUser().getNick())) {
 					URL obj = null;
 					try {
 						obj = new URL("http://pc-logix.com/drama.php?plain");
 					} catch (MalformedURLException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
+						Helper.sendMessage(target, "Something went wrong", nick);
+						return;
 					}
 					try {
 						HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -54,38 +44,48 @@ public class Drama extends AbstractListener {
 						con.setRequestProperty("User-Agent", IRCBot.USER_AGENT);
 						BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 						String inputLine;
-						StringBuffer response = new StringBuffer();
+						StringBuilder response = new StringBuilder();
 						while ((inputLine = in.readLine()) != null) {
 							response.append(inputLine);
 						}
 						in.close();
-						event.getBot().sendIRC().message(event.getChannel().getName(), Helper.antiPing(sender) + ": " + response.toString());
+						Helper.sendMessage(target, response.toString(), nick);
 					} catch (IOException ex) {
-						event.respond("Server returned an error " + ex);
+						Helper.sendMessage(target, "Server returned an error " + ex, nick);
+					} catch (Exception e) {
+						e.printStackTrace();
+						Helper.sendMessage(target, "Something went wrong", nick);
 					}
 				}
-			}			
-		}
+			}
+		};
+		local_command.setHelpText("Generates random drama using Mod Developers, Projects, and other semi random data.");
+		IRCBot.registerCommand(local_command);
 	}
 
+	public String chan;
+	public String target = null;
 
 	@Override
-	public void handleCommand(String nick, GenericMessageEvent event,
-			String command, String[] copyOfRange) {
-		// TODO Auto-generated method stub
-		
+	public void handleCommand(String sender, MessageEvent event, String command, String[] args) {
+		chan = event.getChannel().getName();
 	}
-	
+
+	@Override
+	public void handleCommand(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {
+		if (!event.getClass().getName().equals("org.pircbotx.hooks.events.MessageEvent")) {
+			target = nick;
+		} else {
+			target = chan;
+		}
+		local_command.tryExecute(command, nick, target, event, copyOfRange);
+	}
 
 	@Override
 	public void handleMessage(String sender, MessageEvent event, String command, String[] args) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void handleMessage(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {
-		// TODO Auto-generated method stub
-		
 	}
 }
