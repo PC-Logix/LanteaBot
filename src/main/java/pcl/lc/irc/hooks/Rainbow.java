d4bb3ae9869c5609d87ad84ae081aaac4dd5f9cc
Forecaster <martinek90@hotmail.com>
2017-03-26 13:18:11 +0200

    Migrate Rainbow

--- src/main/java/pcl/lc/irc/hooks/Rainbow.java
+++ src/main/java/pcl/lc/irc/hooks/Rainbow.java
@@ -8,6 +8,7 @@ import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.types.GenericMessageEvent;
 
 import pcl.lc.irc.AbstractListener;
+import pcl.lc.irc.Command;
 import pcl.lc.irc.Config;
 import pcl.lc.irc.IRCBot;
 import pcl.lc.utils.Helper;
@@ -18,71 +19,67 @@ import pcl.lc.utils.Helper;
  */
 @SuppressWarnings("rawtypes")
 public class Rainbow extends AbstractListener {
-	private String chan;
+	private Command local_command;
 
 	public String makeRainbow(String message) {
-
 		Integer rainbow = 0;
-		String messageout = "";
+		String messageOut = "";
 		for (int i = 0; i < message.length(); i++){
 			char c = message.charAt(i);
 			if (rainbow == 0) {
-				messageout = messageout + Colors.RED + c;
+				messageOut = messageOut + Colors.RED + c;
 			} else if (rainbow == 1) {
-				messageout = messageout + Colors.OLIVE + c;
+				messageOut = messageOut + Colors.OLIVE + c;
 			} else if (rainbow == 2) {
-				messageout = messageout + Colors.YELLOW + c;
+				messageOut = messageOut + Colors.YELLOW + c;
 			} else if (rainbow == 3) {
-				messageout = messageout + Colors.GREEN + c;
+				messageOut = messageOut + Colors.GREEN + c;
 			} else if (rainbow == 4) {
-				messageout = messageout + Colors.BLUE + c;
+				messageOut = messageOut + Colors.BLUE + c;
 			} else if (rainbow == 5) {
-				messageout = messageout + Colors.DARK_BLUE + c;
+				messageOut = messageOut + Colors.DARK_BLUE + c;
 			} else if (rainbow == 6) {
-				messageout = messageout + Colors.MAGENTA + c;
+				messageOut = messageOut + Colors.MAGENTA + c;
 			}
 			rainbow++;
 			if (rainbow >= 6) {
 				rainbow = 0;
 			}
 		}
-		return messageout;
+		return messageOut;
 	}
 
 	@Override
 	protected void initHook() {
-		IRCBot.registerCommand("rainbow", "Replies with a rainbow version of the supplied text");
+		local_command = new Command("rainbow", 0) {
+			@Override
+			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, String params) {
+				Helper.sendMessage(target,  Helper.antiPing(nick) + ": " + makeRainbow(params), nick);
+			}
+		}; local_command.setHelpText("Replies with a rainbow version of the supplied text");
+		IRCBot.registerCommand(local_command);
 	}
 
+	public String chan;
+	public String target = null;
 	@Override
 	public void handleCommand(String sender, MessageEvent event, String command, String[] args) {
-		if (command.equals(Config.commandprefix + "rainbow")) {
-			chan = event.getChannel().getName();
-		}
+		chan = event.getChannel().getName();
 	}
 
 	@Override
 	public void handleCommand(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {
-		if (command.equals(Config.commandprefix + "rainbow")) {
-			String target;
-			if (!event.getClass().getName().equals("org.pircbotx.hooks.events.MessageEvent")) {
-				target = nick;
-			} else {
-				target = chan;
-			}
-			String s = event.getMessage().substring(event.getMessage().indexOf("rainbow") + 7).trim();
-			IRCBot.getInstance().sendMessage(target,  Helper.antiPing(nick) + ": " + makeRainbow(s));
+		if (!event.getClass().getName().equals("org.pircbotx.hooks.events.MessageEvent")) {
+			target = nick;
+		} else {
+			target = chan;
 		}
-	}
-	@Override
-	public void handleMessage(String sender, MessageEvent event, String command, String[] args) {
-		// TODO Auto-generated method stub
-		
+		local_command.tryExecute(command, nick, target, event, copyOfRange);
 	}
 
 	@Override
-	public void handleMessage(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void handleMessage(String sender, MessageEvent event, String command, String[] args) {}
+
+	@Override
+	public void handleMessage(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {}
 }
