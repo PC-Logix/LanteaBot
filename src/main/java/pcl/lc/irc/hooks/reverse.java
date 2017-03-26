12efd76fd5a15c02b12942e7b0883fac3e25f2c3
Forecaster <martinek90@hotmail.com>
2017-03-26 14:36:56 +0200

    Migrate Reverse

--- src/main/java/pcl/lc/irc/hooks/reverse.java
+++ src/main/java/pcl/lc/irc/hooks/reverse.java
@@ -8,6 +8,7 @@ import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.types.GenericMessageEvent;
 
 import pcl.lc.irc.AbstractListener;
+import pcl.lc.irc.Command;
 import pcl.lc.irc.Config;
 import pcl.lc.irc.IRCBot;
 import pcl.lc.utils.Helper;
@@ -18,43 +19,40 @@ import pcl.lc.utils.Helper;
  */
 @SuppressWarnings("rawtypes")
 public class reverse extends AbstractListener {
-
-	private String chan;
+	private Command reverse;
 
 	@Override
 	protected void initHook() {
-		IRCBot.registerCommand("reverse", "Reverses the supplied text");
+		reverse = new Command("reverse", 0) {
+			@Override
+			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, String params) {
+				Helper.sendMessage(target, new StringBuffer(Colors.removeFormattingAndColors(params)).reverse().toString(), nick);
+			}
+		};
+		reverse.setHelpText("Reverses the supplied text");
+		IRCBot.registerCommand(reverse);
 	}
 
+	public String chan;
+	public String target = null;
 	@Override
 	public void handleCommand(String sender, MessageEvent event, String command, String[] args) {
-		if (command.equals(Config.commandprefix + "reverse")) {
-			chan = event.getChannel().getName();
-		}
+		chan = event.getChannel().getName();
 	}
 
 	@Override
 	public void handleCommand(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {
-		if (command.equals(Config.commandprefix + "reverse")) {
-			String target;
-			if (!event.getClass().getName().equals("org.pircbotx.hooks.events.MessageEvent")) {
-				target = nick;
-			} else {
-				target = chan;
-			}
-			String s = event.getMessage().substring(event.getMessage().indexOf("reverse") + 7).trim();
-			IRCBot.getInstance().sendMessage(target,  Helper.antiPing(nick) + ": " + new StringBuffer(Colors.removeFormattingAndColors(s)).reverse().toString());
+		if (!event.getClass().getName().equals("org.pircbotx.hooks.events.MessageEvent")) {
+			target = nick;
+		} else {
+			target = chan;
 		}
-	}
-	@Override
-	public void handleMessage(String sender, MessageEvent event, String command, String[] args) {
-		// TODO Auto-generated method stub
-		
+		reverse.tryExecute(command, nick, target, event, copyOfRange);
 	}
 
 	@Override
-	public void handleMessage(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {
-		// TODO Auto-generated method stub
-		
-	}
+	public void handleMessage(String sender, MessageEvent event, String command, String[] args) {}
+
+	@Override
+	public void handleMessage(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {}
 }
