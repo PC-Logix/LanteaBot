ed8573b9df17440143069f20605913aa7095e437
Forecaster <martinek90@hotmail.com>
2017-04-04 18:29:20 +0200

    Add WhatIsLove hook at Inari's suggestion

---
+++ src/main/java/pcl/lc/irc/hooks/WhatIsLove.java
@@ -0,0 +1,57 @@
+package pcl.lc.irc.hooks;
+
+import org.pircbotx.hooks.events.MessageEvent;
+import org.pircbotx.hooks.types.GenericMessageEvent;
+import pcl.lc.irc.AbstractListener;
+import pcl.lc.irc.Command;
+import pcl.lc.irc.IRCBot;
+import pcl.lc.utils.Helper;
+
+/**
+ * @author Caitlyn
+ *
+ */
+@SuppressWarnings("rawtypes")
+public class WhatIsLove extends AbstractListener {
+	private Command local_command;
+
+	@Override
+	protected void initHook() {
+		initCommands();
+		IRCBot.registerCommand(local_command, "The answer to the most question");
+	}
+
+	private void initCommands() {
+		local_command = new Command("whatislove", 0) {
+			@Override
+			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, String params) {
+				int roll = Helper.getRandomInt(0,100);
+				Helper.sendMessage(target, "Love is... " + Inventory.getRandomItem(true).getName() + ((roll < 25) ? ", with " + Inventory.getRandomItem(false).getName() + " on top!" : "!"), nick);
+			}
+		};
+		local_command.registerAlias("loveis");
+	}
+
+	public String chan;
+	public String target = null;
+	@Override
+	public void handleCommand(String sender, MessageEvent event, String command, String[] args) {
+		chan = event.getChannel().getName();
+	}
+
+	@Override
+	public void handleCommand(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {
+		if (!event.getClass().getName().equals("org.pircbotx.hooks.events.MessageEvent")) {
+			target = nick;
+		} else {
+			target = chan;
+		}
+		local_command.tryExecute(command, nick, target, event, copyOfRange);
+	}
+
+	@Override
+	public void handleMessage(String sender, MessageEvent event, String command, String[] args) {}
+
+	@Override
+	public void handleMessage(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {}
+}
