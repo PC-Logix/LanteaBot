ddc930350cc9ae73d16b53fcfedd43757aa5c793
Caitlyn Mainer <Michiyo.Ravencroft@gmail.com>
2017-05-09 11:26:14 -0500

    good enough ._.

--- src/main/java/pcl/lc/utils/InputThread.java
+++ src/main/java/pcl/lc/utils/InputThread.java
@@ -4,6 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.lang3.ArrayUtils;
 
 import pcl.lc.irc.IRCBot;
 
@@ -12,11 +17,11 @@ public class InputThread extends Thread
 	boolean done = false;
 	// reader to read keyboard input 
 	private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
- 
+
 	// writer to send data down the socket 
 	private PrintWriter writer; 
- 
- 
+
+
 	public void run() 
 	{ 
 		while(!done) 
@@ -28,11 +33,15 @@ public class InputThread extends Thread
 				String[] split = line.split("\\s+");
 				String command = split[0];
 				String target = split[1];
-				split[1] = "";
-				split[0] = "";
+				final List<String> list =  new ArrayList<String>();
+				Collections.addAll(list, split); 
+				list.remove(command);
+				list.remove(target);
+				split = list.toArray(new String[list.size()]);
+
 				StringBuilder builder = new StringBuilder();
 				for(String s : split) {
-				    builder.append(s + " ");
+					builder.append(s + " ");
 				}
 				String message = builder.toString();
 				if (command.equals("msg")) {
