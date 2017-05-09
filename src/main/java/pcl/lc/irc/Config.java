15dbb7438d9b5005d1d29463c5ac8bf27022a594
Caitlyn Mainer <Michiyo.Ravencroft@gmail.com>
2017-05-09 14:45:14 -0500

    set realname, and ident

--- src/main/java/pcl/lc/irc/Config.java
+++ src/main/java/pcl/lc/irc/Config.java
@@ -123,7 +123,7 @@ public class Config {
 				System.setProperty("socksProxyPort",Config.proxyport);
 			}
 
-			Config.config.setName(Config.nick).setLogin("lb");
+			Config.config.setRealName(Config.nick).setName(Config.nick).setLogin(Config.nick);
 			Config.config.setAutoNickChange(true);
 			Config.config.setCapEnabled(true);
 			Config.config.setAutoReconnect(true);
@@ -141,6 +141,7 @@ public class Config {
 			if (Config.enableTLS.equals("true")) {
 				Config.config.addCapHandler(new TLSCapHandler(new UtilSSLSocketFactory().trustAllCertificates(), true));
 			} 
+			Config.config.setSnapshotsEnabled(false);
 			if (Config.enableSSL.equals("true")) {
 				Config.config.addServer(Config.botConfig.get("server").toString(), Integer.parseInt(Config.botConfig.get("serverport").toString()))
 			    .setSocketFactory(new UtilSSLSocketFactory().trustAllCertificates()).setServerPassword(Config.botConfig.get("serverpass").toString());
