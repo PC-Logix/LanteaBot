1d96f972700b29521b9eb2f5944e2f44613f3a67
Caitlyn Mainer <Michiyo.Ravencroft@gmail.com>
2017-11-29 09:05:26 -0600

    Add dynamic antiping to Fling... let's see if it works!

--- src/main/java/pcl/lc/irc/hooks/Fling.java
+++ src/main/java/pcl/lc/irc/hooks/Fling.java
@@ -33,6 +33,7 @@ public class Fling extends AbstractListener {
 
 				if (item != null) {
 					String user = Helper.getRandomUser(event);
+					Helper.AntiPings = Helper.getNamesFromTarget(target);
 					Helper.sendAction(target, "flings " + item.getName() + " in a random direction. It hits " + user + " " + Helper.get_hit_place() + ". They take " + Helper.rollDiceString("1d6") + " damage.");
 					String dust = item.decrementUses();
 					if (dust != "")
