6271c8e6865f7d4520808deaca151560c50a0af9
Caitlyn Mainer <Michiyo.Ravencroft@gmail.com>
2017-12-19 21:48:40 -0600

    dyncommands can now store and run Lua and Javascript

--- src/main/java/pcl/lc/irc/hooks/DynamicCommands.java
+++ src/main/java/pcl/lc/irc/hooks/DynamicCommands.java
@@ -6,169 +6,369 @@ package pcl.lc.irc.hooks;
 import org.pircbotx.Colors;
 import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.types.GenericMessageEvent;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import com.naef.jnlua.JavaFunction;
+import com.naef.jnlua.LuaException;
+import com.naef.jnlua.LuaState;
+import com.naef.jnlua.LuaState.Library;
+
+import jdk.nashorn.api.scripting.NashornScriptEngine;
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
 import pcl.lc.irc.*;
+import pcl.lc.irc.hooks.JavaScript.JSRunner;
 import pcl.lc.utils.Database;
 import pcl.lc.utils.Helper;
 import pcl.lc.utils.Item;
+import pcl.lc.utils.SandboxThreadFactory;
+import pcl.lc.utils.SandboxThreadGroup;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.MessageFormat;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import javax.script.CompiledScript;
+import javax.script.ScriptContext;
+import javax.script.ScriptException;
 
 /**
  * @author Caitlyn
  */
 @SuppressWarnings("rawtypes")
 public class DynamicCommands extends AbstractListener {
-  private Command local_command_add;
-  private Command local_command_del;
-  private Command local_command_addhelp;
+	private NashornScriptEngineFactory engineFactory = new NashornScriptEngineFactory();;
+	private final SandboxThreadGroup sandboxGroup = new SandboxThreadGroup("javascript");
+	private final ThreadFactory sandboxFactory = new SandboxThreadFactory(sandboxGroup);
+	
+	private Command local_command_add;
+	private Command local_command_del;
+	private Command local_command_addhelp;
 
-  @Override
-  protected void initHook() {
-    local_command_add = new Command("addcommand", 0);
-    IRCBot.registerCommand(local_command_add, "Adds a dynamic command to the bot, requires BotAdmin, or Channel Op.");
-    local_command_del = new Command("delcommand", 0);
-    local_command_addhelp = new Command ("addcommandhelp", 0) {
-		@Override
-		public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, String params) {
-			PreparedStatement addCommandHelp;
-			try {
-				addCommandHelp = Database.getPreparedStatement("addCommandHelp");
-				String arr[] = params.split(" ", 2);
-				String theCommand = arr[0];
-				String theHelp = arr[1]; 
-				if (IRCBot.commands.containsKey(theCommand)) {
-					try {
-						addCommandHelp.setString(1, theHelp);
-						addCommandHelp.setString(2, theCommand.toLowerCase());
-						addCommandHelp.executeUpdate();
-						IRCBot.setHelp(theCommand, theHelp);
-						event.respond("Help Set");
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-						event.respond("fail 1");
+	private Command local_command_print;
+	private Command local_command_edit;
+
+	public String luasb;
+
+	private static LuaState luaState;
+	public StringBuilder output;
+		
+	@Override
+	protected void initHook() {
+		InputStream luain = getClass().getResourceAsStream("/jnlua/luasb.lua");
+		try {
+			luasb = CharStreams.toString(new InputStreamReader(luain, Charsets.UTF_8));
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+
+		initLua();
+		local_command_add = new Command("addcommand", 0);
+		IRCBot.registerCommand(local_command_add, "Adds a dynamic command to the bot, requires BotAdmin, or Channel Op.");
+		local_command_del = new Command("delcommand", 0);
+		local_command_addhelp = new Command ("addcommandhelp", 0) {
+			@Override
+			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, String params) {
+				PreparedStatement addCommandHelp;
+				try {
+					addCommandHelp = Database.getPreparedStatement("addCommandHelp");
+					String arr[] = params.split(" ", 2);
+					String theCommand = arr[0];
+					String theHelp = arr[1]; 
+					if (IRCBot.commands.containsKey(theCommand)) {
+						try {
+							addCommandHelp.setString(1, theHelp);
+							addCommandHelp.setString(2, theCommand.toLowerCase());
+							addCommandHelp.executeUpdate();
+							IRCBot.setHelp(theCommand, theHelp);
+							event.respond("Help Set");
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+							event.respond("fail 1");
+						}
+					} else {
+						event.respond("fail 2 ");
 					}
-				} else {
-					event.respond("fail 2 ");
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+					event.respond("fail 3");
 				}
-			} catch (Exception e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-				event.respond("fail 3");
+			}
+		};
+		local_command_addhelp.setHelpText("Sets help on dynamic commands");
+		IRCBot.registerCommand(local_command_del, "Removes a dynamic command to the bot, requires BotAdmin, or Channel Op.");
+		IRCBot.registerCommand(local_command_addhelp);
+		Database.addStatement("CREATE TABLE IF NOT EXISTS Commands(command STRING UNIQUE PRIMARY KEY, return, help)");
+		Database.addUpdateQuery(5, "ALTER TABLE Commands ADD help STRING DEFAULT NULL NULL;");
+		Database.addPreparedStatement("addCommand", "INSERT INTO Commands(command, return) VALUES (?, ?);");
+		Database.addPreparedStatement("addCommandHelp", "UPDATE Commands SET help = ? WHERE command = ?");
+		Database.addPreparedStatement("searchCommands", "SELECT command, help FROM Commands");
+		Database.addPreparedStatement("getCommand", "SELECT return FROM Commands WHERE command = ?");
+		Database.addPreparedStatement("delCommand", "DELETE FROM Commands WHERE command = ?;");
+		try {
+			PreparedStatement searchCommands = Database.getPreparedStatement("searchCommands");
+			ResultSet commands = searchCommands.executeQuery();
+			while (commands.next()) {
+				if (commands.getString(2) != null) {
+					IRCBot.registerCommand(commands.getString(1), commands.getString(2));
+				} else {
+					IRCBot.registerCommand(commands.getString(1), "Dynamic commands module, who knows what it does?!");
+				}
 			}
 		}
-    };
-    local_command_addhelp.setHelpText("Sets help on dynamic commands");
-    IRCBot.registerCommand(local_command_del, "Removes a dynamic command to the bot, requires BotAdmin, or Channel Op.");
-    IRCBot.registerCommand(local_command_addhelp);
-    Database.addStatement("CREATE TABLE IF NOT EXISTS Commands(command STRING UNIQUE PRIMARY KEY, return, help)");
-    Database.addUpdateQuery(5, "ALTER TABLE Commands ADD help STRING DEFAULT NULL NULL;");
-    Database.addPreparedStatement("addCommand", "INSERT INTO Commands(command, return) VALUES (?, ?);");
-    Database.addPreparedStatement("addCommandHelp", "UPDATE Commands SET help = ? WHERE command = ?");
-    Database.addPreparedStatement("searchCommands", "SELECT command, help FROM Commands");
-    Database.addPreparedStatement("getCommand", "SELECT return FROM Commands WHERE command = ?");
-    Database.addPreparedStatement("delCommand", "DELETE FROM Commands WHERE command = ?;");
-    try {
-      PreparedStatement searchCommands = Database.getPreparedStatement("searchCommands");
-      ResultSet commands = searchCommands.executeQuery();
-      while (commands.next()) {
-    	  if (commands.getString(2) != null) {
-    	       IRCBot.registerCommand(commands.getString(1), commands.getString(2));
-    	  } else {
-    	       IRCBot.registerCommand(commands.getString(1), "Dynamic commands module, who knows what it does?!");
-    	  }
-       }
-    }
-    catch (Exception e) {
-      e.printStackTrace();
-      IRCBot.log.info("An error occurred while processing this command");
-    }
-  }
-  
+		catch (Exception e) {
+			e.printStackTrace();
+			IRCBot.log.info("An error occurred while processing this command");
+		}
+	}
+
 	public String chan;
 	public String target = null;
-  @Override
-  public void handleCommand(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {
-	  String prefix = Config.commandprefix;
-	  target = Helper.getTarget(event);
-	  try {
-          PreparedStatement getCommand = Database.getPreparedStatement("getCommand");
-          getCommand.setString(1, command.replace(prefix, "").toLowerCase());
-          ResultSet command1 = getCommand.executeQuery();
-          if (command1.next()) {
-            String msg = MessageFormat.format(command1.getString(1), copyOfRange);
-            if (msg.contains("[randomitem]")) {
-            	msg = msg.replace("[randomitem]", Inventory.getRandomItem().getName());
-            }
-            if (msg.contains("[drama]")) {
-            	msg = msg.replace("[drama]", Drama.dramaParse());
-            }
-            event.getBot().sendIRC().message(target, msg);
-          }
-        }
-        catch (Exception e) {
-          e.printStackTrace();
-          System.out.println("An error occurred while processing this command");
-        }
-  }
+	@Override
+	public void handleCommand(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {
+		String prefix = Config.commandprefix;
+		target = Helper.getTarget(event);
+		try {
+			PreparedStatement getCommand = Database.getPreparedStatement("getCommand");
+			getCommand.setString(1, command.replace(prefix, "").toLowerCase());
+			ResultSet command1 = getCommand.executeQuery();
+			output = new StringBuilder();
+			if (command1.next()) {
+				String message = command1.getString(1);
+				if (message.startsWith("[lua]")) {
+					output = new StringBuilder();
+					output.append(runScriptInSandbox(message.replace("[lua]", "").trim()));
+					message = output.toString();
+				}
+				if (message.startsWith("[js]")) {
+					if (engineFactory == null) return;
+					NashornScriptEngine engine = (NashornScriptEngine)engineFactory.getScriptEngine(new String[] {"-strict", "--no-java", "--no-syntax-extensions"});
+					output = new StringBuilder();
+					output.append(eval(engine, message.replace("[js]", "").trim()));
+					message = output.toString().replace("\n", " | ").replace("\r", "");
+				}
+				String msg = "";
+				if (message.contains("[randomitem]")) {
+					msg = MessageFormat.format(command1.getString(1), message);
+					message = msg.replace("[randomitem]", Inventory.getRandomItem().getName());
+				}
+				if (message.contains("[drama]")) {
+					msg = MessageFormat.format(command1.getString(1), message);
+					message = msg.replace("[drama]", Drama.dramaParse());
+				}
+				Helper.AntiPings = Helper.getNamesFromTarget(target);
+				Helper.sendMessage(target, message);
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("An error occurred while processing this command");
+		}
+	}
 
-  @Override
-  public void handleCommand(String sender, MessageEvent event, String command, String[] args) {
-    
-    String ourinput = event.getMessage().toLowerCase().trim();
-    chan = event.getChannel().getName();
-	target = Helper.getTarget(event);
-    local_command_addhelp.tryExecute(command, sender, event.getChannel().getName(), event, args);
-    if (ourinput.length() > 1) {
-      if (!IRCBot.isIgnored(event.getUser().getNick())) {
-        String[] message = event.getMessage().split(" ", 3);
+	@Override
+	public void handleCommand(String sender, MessageEvent event, String command, String[] args) {
 
-        long shouldExecute = local_command_add.shouldExecute(command, event);
-        if (shouldExecute == 0) {
-          boolean isOp = Permissions.isOp(event.getBot(), event.getUser());
-          if (isOp || Helper.isChannelOp(event)) {
-            try {
-              PreparedStatement addCommand = Database.getPreparedStatement("addCommand");
-              if (!IRCBot.commands.containsKey(message[1])) {
-                addCommand.setString(1, message[1].toLowerCase());
-                addCommand.setString(2, message[2]);
-                addCommand.executeUpdate();
-                event.respond("Command Added");
-                IRCBot.registerCommand(message[1].toLowerCase(), "Dynamic commands module, who knows what it does?!");
-              }
-              else {
-                event.respond("Can't override existing commands.");
-              }
-            }
-            catch (Exception e) {
-              e.printStackTrace();
-              event.respond("An error occurred while processing this command");
-            }
-          }
-        }
-        else {
-          shouldExecute = local_command_del.shouldExecute(command, event);
-          if (shouldExecute == 0) {
-            boolean isOp = Permissions.isOp(event.getBot(), event.getUser());
-            if (isOp || Helper.isChannelOp(event)) {
-              try {
-                PreparedStatement delCommand = Database.getPreparedStatement("delCommand");
-                delCommand.setString(1, message[1].toLowerCase());
-                delCommand.execute();
-                event.respond("Command deleted");
-                IRCBot.unregisterCommand(message[1].toLowerCase());
-              }
-              catch (Exception e) {
-                e.printStackTrace();
-                event.respond("An error occurred while processing this command");
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+		String ourinput = event.getMessage().toLowerCase().trim();
+		chan = event.getChannel().getName();
+		target = Helper.getTarget(event);
+		local_command_addhelp.tryExecute(command, sender, event.getChannel().getName(), event, args);
+		//local_command_print.tryExecute(command, sender, event.getChannel().getName(), event, args);
+		//local_command_edit.tryExecute(command, sender, event.getChannel().getName(), event, args);
+		if (ourinput.length() > 1) {
+			if (!IRCBot.isIgnored(event.getUser().getNick())) {
+				String[] message = event.getMessage().split(" ", 3);
+
+				long shouldExecute = local_command_add.shouldExecute(command, event);
+				if (shouldExecute == 0) {
+					boolean isOp = Permissions.isOp(event.getBot(), event.getUser());
+					if (isOp || Helper.isChannelOp(event)) {
+						try {
+							PreparedStatement addCommand = Database.getPreparedStatement("addCommand");
+							if (!IRCBot.commands.containsKey(message[1])) {
+								addCommand.setString(1, message[1].toLowerCase());
+								addCommand.setString(2, message[2]);
+								addCommand.executeUpdate();
+								event.respond("Command Added");
+								IRCBot.registerCommand(message[1].toLowerCase(), "Dynamic commands module, who knows what it does?!");
+							}
+							else {
+								event.respond("Can't override existing commands.");
+							}
+						}
+						catch (Exception e) {
+							e.printStackTrace();
+							event.respond("An error occurred while processing this command");
+						}
+					}
+				}
+				else {
+					shouldExecute = local_command_del.shouldExecute(command, event);
+					if (shouldExecute == 0) {
+						boolean isOp = Permissions.isOp(event.getBot(), event.getUser());
+						if (isOp || Helper.isChannelOp(event)) {
+							try {
+								PreparedStatement delCommand = Database.getPreparedStatement("delCommand");
+								delCommand.setString(1, message[1].toLowerCase());
+								delCommand.execute();
+								event.respond("Command deleted");
+								IRCBot.unregisterCommand(message[1].toLowerCase());
+							}
+							catch (Exception e) {
+								e.printStackTrace();
+								event.respond("An error occurred while processing this command");
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	private static String stackToString(LuaState luaState) {
+		int top = luaState.getTop();
+		if (top > 0) {
+			StringBuilder results = new StringBuilder();
+			for (int i = 1; i <= top; i++) {
+				String result;
+				if (luaState.isString(i) || luaState.isNumber(i))
+					result = luaState.toString(i);
+				else if (luaState.isBoolean(i))
+					result = luaState.toBoolean(i) ? "true" : "false";
+				else if (luaState.isNil(i))
+					result = luaState.typeName(i);
+				else
+					result = String.format("%s: 0x%x", luaState.typeName(i), luaState.toPointer(i));
+				results.append(result);
+				if (i < top)
+					results.append(", ");
+			}
+			return results.toString();
+		} else {
+			return "";
+		}
+	}
+
+	protected void initLua() {
+		if (luaState != null) {
+			luaState.close();
+		}
+		luaState = new LuaState(1024*1024);
+		luaState.openLib(Library.BASE);
+		luaState.openLib(Library.COROUTINE);
+		luaState.openLib(Library.TABLE);
+		luaState.openLib(Library.STRING);
+		luaState.openLib(Library.BIT32);
+		luaState.openLib(Library.MATH);
+		luaState.openLib(Library.OS);
+		luaState.openLib(Library.PACKAGE);
+		luaState.openLib(Library.DEBUG);
+		luaState.setTop(0); // Remove tables from loaded libraries
+		luaState.pushJavaFunction(new JavaFunction() {
+			@Override
+			public int invoke(LuaState luaState) {
+				String results = stackToString(luaState);
+				output.append(results + "\n");
+				return 0;
+			}
+		});
+		luaState.setGlobal("print");
+		
+		luaState.load(luasb, "=luasb");
+		luaState.call(0, 0);
+	}
+
+	static String runScriptInSandbox(String script) {
+		luaState.setTop(0); // Ensure stack is clean
+		luaState.getGlobal("lua");
+		luaState.pushString(script);
+		try {
+			luaState.call(1, LuaState.MULTRET);
+		} catch (LuaException error) {
+			luaState.setTop(0);
+			return error.getMessage();
+		}
+		String results = stackToString(luaState);
+		luaState.setTop(0); // Remove results from stack
+		return results.toString();
+	}
+	
+	public String eval(NashornScriptEngine engine, String code) {
+		CompiledScript cs;
+		try {
+			cs = engine.compile(code);
+		} catch (ScriptException e) {
+			return e.getMessage();
+		}
+		JSRunner r = new JSRunner(cs);
+
+		final ExecutorService service = Executors.newSingleThreadExecutor(sandboxFactory);
+		TimeUnit unit = TimeUnit.SECONDS;
+		String output = null;
+		try {
+			Future<String> f = service.submit(r);
+		    output = f.get(5, unit);
+		}
+		catch(TimeoutException e) {
+		    output = "Script timed out";
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			output = e.getMessage();
+		}
+		finally {
+		    service.shutdown();
+		}
+		if (output == null)
+			output = "";
+
+		return output;
+	}
+
+	public class JSRunner implements Callable<String> {
+		
+		private final CompiledScript cs;
+		
+		public JSRunner(CompiledScript cs) {
+			this.cs = cs;
+		}
+
+		@Override
+		public String call() throws Exception {
+				StringWriter sw = new StringWriter();
+				PrintWriter pw = new PrintWriter(sw);
+				ScriptContext context = cs.getEngine().getContext();
+				context.setWriter(pw);
+				context.setErrorWriter(pw);
+				
+				try {
+					Object out = cs.eval();
+					if (sw.getBuffer().length() != 0)
+						return sw.toString();
+					if (out != null)
+						return out.toString();
+				}
+				catch(ScriptException ex) {
+					return ex.getMessage();
+				}
+				return null;
+		}
+	}
+	
 }
