f7e88f60f93b4e0eb12ffac1865907b2df8b019b
Forecaster <martinek90@hotmail.com>
2018-07-25 21:47:32 +0200

    - Add exemptions  - Param checks

--- src/main/java/pcl/lc/irc/hooks/PhraseBan.java
+++ src/main/java/pcl/lc/irc/hooks/PhraseBan.java
@@ -25,6 +25,8 @@ public class PhraseBan extends AbstractListener {
 	private Command del;
 	private Command list;
 	private Command clear;
+	private Command exadd;
+	private Command exdel;
 
 	private ArrayList<String> phrases;
 
@@ -33,6 +35,7 @@ public class PhraseBan extends AbstractListener {
 		initCommands();
 		IRCBot.registerCommand(local_command);
 		Database.addStatement("CREATE TABLE IF NOT EXISTS BannedPhrases(id INTEGER PRIMARY KEY, phrase VARCHAR(200), channel VARCHAR(200))");
+		Database.addStatement("CREATE TABLE IF NOT EXISTS ExemptNicks(id INTEGER PRIMARY KEY, nick VARCHAR(200), channel VARCHAR(200))");
 
 		try {
 			phrases = new ArrayList<>();
@@ -61,9 +64,18 @@ public class PhraseBan extends AbstractListener {
 		add = new Command("add", 0, Permissions.MOD) {
 			@Override
 			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, String params) {
+				if (params.isEmpty()) {
+					Helper.sendMessage(target, "You must specify a nick.", nick);
+					return;
+				}
 				Statement statement;
 				try {
 					statement = Database.getConnection().createStatement();
+					ResultSet result = statement.executeQuery("SELECT * FROM BannedPhrases WHERE phrase = '" + params.toLowerCase() + "'");
+					if (result.next()) {
+						Helper.sendMessage(target, "Phrase already banned.", nick);
+						return;
+					}
 					statement.executeUpdate("INSERT INTO BannedPhrases (phrase) VALUES ('" + params.toLowerCase() + "')");
 					Helper.sendMessage(target, "Added phrase to banlist", nick);
 					phrases.add(params.toLowerCase());
@@ -77,6 +89,10 @@ public class PhraseBan extends AbstractListener {
 		del = new Command("del", 0, Permissions.MOD) {
 			@Override
 			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, String params) {
+				if (params.isEmpty()) {
+					Helper.sendMessage(target, "You must specify a nick.", nick);
+					return;
+				}
 				Statement statement;
 				try {
 					statement = Database.getConnection().createStatement();
@@ -88,6 +104,7 @@ public class PhraseBan extends AbstractListener {
 				}
 			}
 		};
+		del.registerAlias("rem");
 		local_command.registerSubCommand(del);
 
 		list = new Command("list", 0, Permissions.EVERYONE) {
@@ -112,6 +129,48 @@ public class PhraseBan extends AbstractListener {
 			}
 		};
 		local_command.registerSubCommand(clear);
+
+		exadd = new Command("exadd", 0, Permissions.MOD) {
+			@Override
+			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, ArrayList<String> params) {
+				if (params.isEmpty()) {
+					Helper.sendMessage(target, "You must specify a nick.", nick);
+					return;
+				}
+				try {
+					Statement statement = Database.getConnection().createStatement();
+					ResultSet result = statement.executeQuery("SELECT * FROM ExemptNicks WHERE nick = '" + params.get(0) + "'");
+					if (result.next()) {
+						Helper.sendMessage(target, "Nick already exempt.", nick);
+						return;
+					}
+					statement.executeUpdate("INSERT INTO ExemptNicks (nick) VALUES ('" + params.get(0) + "')");
+					Helper.sendMessage(target, "Added to exempt list!", nick);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		local_command.registerSubCommand(exadd);
+
+		exdel = new Command("exdel", 0, Permissions.MOD) {
+			@Override
+			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, ArrayList<String> params) {
+				if (params.isEmpty()) {
+					Helper.sendMessage(target, "You must specify a nick.", nick);
+					return;
+				}
+				try {
+					Statement statement = Database.getConnection().createStatement();
+					statement.executeUpdate("DELETE FROM ExemptNicks WHERE nick = '" + params.get(0) + "'");
+					Helper.sendMessage(target, "Removed from exempt list");
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		exdel.registerAlias("exrem");
+		local_command.registerSubCommand(exdel);
 	}
 
 	public String chan;
@@ -135,14 +194,25 @@ public class PhraseBan extends AbstractListener {
 	@Override
 	public void handleMessage(String nick, GenericMessageEvent event, String[] copyOfRange) {
 		String message = event.getMessage();
+		try {
+			Statement statement = Database.getConnection().createStatement();
+			ResultSet result = statement.executeQuery("SELECT * FROM ExemptNicks WHERE nick = '" + nick + "'");
+			if (result.next())
+				return;
 
-		if (!Permissions.hasPermission(IRCBot.bot, event, Permissions.MOD)) {
+			if (Permissions.hasPermission(IRCBot.bot, event, Permissions.MOD))
+				return;
+
 			for (String phrase : phrases) {
 				if (message.toLowerCase().contains(phrase)) {
 					Helper.sendMessage("chanserv", "kickban " + target + " " + nick + " Banned phrase '" + phrase + "'");
+					if (IRCBot.getOurNick().equals("ForeBot"))
+						Helper.sendMessage(target, "BAN!");
 					break;
 				}
 			}
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
 	}
 }
