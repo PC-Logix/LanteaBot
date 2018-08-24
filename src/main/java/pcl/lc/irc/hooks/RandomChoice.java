ef88c3c8fb3d380e9c051198fd207a741cd3ac42
Forecaster <martinek90@hotmail.com>
2018-08-24 11:20:28 +0200

    Implement minimum arguments, to prevent outputting all choices in a reply

--- src/main/java/pcl/lc/irc/hooks/RandomChoice.java
+++ src/main/java/pcl/lc/irc/hooks/RandomChoice.java
@@ -10,37 +10,85 @@ import pcl.lc.utils.Helper;
 import java.util.ArrayList;
 import java.util.List;
 
+class TemplateCollection {
+	List<Template> templates;
+
+	TemplateCollection() {
+		this.templates = new ArrayList<>();
+	}
+
+	public void add(Template template) {
+		this.templates.add(template);
+	}
+
+	public Template getRandomTemplate() {
+		return getRandomTemplate(1);
+	}
+
+	public Template getRandomTemplate(int min_arguments) {
+		System.out.println("Looking for template with min arguments " + min_arguments);
+		int rand = 0;
+
+		Template template = null;
+		while (template == null || template.min_arguments > min_arguments) {
+			rand = Helper.getRandomInt(0, this.templates.size() - 1);
+			template = this.templates.get(rand);
+			System.out.println("Found template with " + template.min_arguments + " min arguments");
+		}
+		return template;
+	}
+}
+
+class Template {
+	public int min_arguments;
+	public String template;
+
+	Template(String template) {
+		this.min_arguments = 1;
+		this.template = template;
+	}
+
+	Template(int min_arguments, String template) {
+		this.min_arguments = min_arguments;
+		this.template = template;
+	}
+}
+
 public class RandomChoice extends AbstractListener {
 	private Command local_command;
-	private List<String> output;
+	private TemplateCollection templates;
 
 	@Override
 	protected void initHook() {
-		output = new ArrayList<>();
-		output.add("Some {choice} sounds nice");
-		output.add("I'm 40% {choice}!");
-		output.add("You *could* do {choice}, I guess.");
-		output.add("Why not {count}?");
-		output.add("I sense some {choice} in your future!");
-		output.add("{choice} is for cool kids!");
-		output.add("Definitely {choice}. or maybe {other_choice}...");
-		output.add("If I had a gold nugget for every time someone asked me about {choice}");
-		output.add("The proof is in the pudding. Definitely {choice}.");
-		output.add("I received a message from future you, said to go with {choice}.");
-		output.add("I was that {choice} is the best choice in a vision");
-		output.add("You'll want to go with {choice}.");
-		output.add("Elementary dear Watson, {choice} is the obvious choice!");
-		output.add("My grandfather always told me that {choice} is the way to go!");
-		output.add("If I've learned anything in life it's that you always pick {choice}");
-		output.add("Once you get a taste of {choice} you can't stop.");
-		output.add("One the one hand, there's {choice} but then there's also {other_choice}");
-		output.add("Somebody once told me to roll with {choice}");
+		templates = new TemplateCollection();
+		templates.add(new Template("Some {choice} sounds nice"));
+		templates.add(new Template("I'm 40% {choice}!"));
+		templates.add(new Template("You *could* do {choice}, I guess."));
+		templates.add(new Template("Why not {count}?"));
+		templates.add(new Template("I sense some {choice} in your future!"));
+		templates.add(new Template("{choice} is for cool kids!"));
+		templates.add(new Template(3, "Definitely {choice}. or maybe {other_choice}..."));
+		templates.add(new Template("If I had a gold nugget for every time someone asked me about {choice}"));
+		templates.add(new Template("The proof is in the pudding. Definitely {choice}."));
+		templates.add(new Template("I received a message from future you, said to go with {choice}."));
+		templates.add(new Template("I was that {choice} is the best choice in a vision"));
+		templates.add(new Template("You'll want to go with {choice}."));
+		templates.add(new Template("Elementary dear Watson, {choice} is the obvious choice!"));
+		templates.add(new Template("My grandfather always told me that {choice} is the way to go!"));
+		templates.add(new Template("If I've learned anything in life it's that you always pick {choice}"));
+		templates.add(new Template("Once you get a taste of {choice} you can't stop."));
+		templates.add(new Template(3, "One the one hand, there's {choice} but then there's also {other_choice}"));
+		templates.add(new Template("Somebody once told me to roll with {choice}"));
+		templates.add(new Template("Out of these {raw_count} choices? I'd say {choice}."));
+		templates.add(new Template("I've heard {choice} is in these days"));
+		templates.add(new Template("I spy with my robotic eye something beginning with {choice}!"));
 
 		local_command = new Command("choose", 0) {
 			@Override
 			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, String params) {
 				String[] parts = params.split(" or ");
-				String msg = output.get(Helper.getRandomInt(0, output.size() - 1));
+//				String msg = output.get(Helper.getRandomInt(0, output.size() - 1));
+				String msg = templates.getRandomTemplate(parts.length).template;
 
 				String choice = parts[Helper.getRandomInt(0, parts.length - 1)].trim();
 				msg = msg.replaceAll("\\{choice}", choice);
@@ -54,33 +102,54 @@ public class RandomChoice extends AbstractListener {
 				else
 					msg = msg.replaceAll("\\{other_choice}", "something else");
 				String count = "";
+				String raw_count = "";
 				switch (parts.length)
 				{
 					case 1:
 						msg = "The choice seems obvious..."; break;
 					case 2:
-						count = "both"; break;
+						count = "both";
+						raw_count = "two";
+						break;
 					case 3:
-						count = "all three"; break;
+						count = "all three";
+						raw_count = "three";
+						break;
 					case 4:
-						count = "all four"; break;
+						count = "all four";
+						raw_count = "four";
+						break;
 					case 5:
-						count = "all five"; break;
+						count = "all five";
+						raw_count = "five";
+						break;
 					case 6:
-						count = "all six"; break;
+						count = "all six";
+						raw_count = "six";
+						break;
 					case 7:
-						count = "all seven"; break;
+						count = "all seven";
+						raw_count = "seven";
+						break;
 					case 8:
-						count = "all eight"; break;
+						count = "all eight";
+						raw_count = "eight";
+						break;
 					case 9:
-						count = "all nine"; break;
+						count = "all nine";
+						raw_count = "nine";
+						break;
 					case 10:
-						count = "all ten"; break;
+						count = "all ten";
+						raw_count = "ten";
+						break;
 					default:
 						count = "all " + parts.length;
+						raw_count = "" + parts.length;
 				}
 				if (count != "")
 					msg = msg.replaceAll("\\{count}", count);
+				msg = msg.replaceAll("\\{raw_count}", raw_count);
 
 				Helper.sendMessage(target, msg, nick);
 			}
