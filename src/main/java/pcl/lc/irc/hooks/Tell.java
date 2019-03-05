970094877ca16ce0418a34021405b3aa4adfe837
Caitlyn Mainer <Michiyo.Ravencroft@gmail.com>
2019-03-05 09:28:46 -0800

    pluralizer

--- src/main/java/pcl/lc/irc/hooks/Tell.java
+++ src/main/java/pcl/lc/irc/hooks/Tell.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 
 import org.apache.commons.lang3.StringUtils;
+import org.jvnet.inflector.Noun;
 import org.pircbotx.hooks.events.JoinEvent;
 import org.pircbotx.hooks.events.MessageEvent;
 import org.pircbotx.hooks.types.GenericMessageEvent;
@@ -25,29 +26,25 @@ public class Tell extends AbstractListener {
 	public String dest;
 
 	public String chan;
-	
+
 	@Override
 	public void onJoin(final JoinEvent event) {
 		if (event.getUser().getNick().equals(IRCBot.getOurNick()))
 			return;
 		int numTells = 0;
-        try {
-            PreparedStatement checkTells = Database.getPreparedStatement("getTells");
-            checkTells.setString(1, event.getUser().getNick().toLowerCase());
-            ResultSet results = checkTells.executeQuery();
-            while (results.next()) {
-            	numTells++;
-            }
-            if (numTells > 0) {
-		if (numTells > 1) {
-		    event.getUser().send().notice("You have " + numTells + " tells currently waiting for you.");
-		} else {
-		    event.getUser().send().notice("You have " + numTells + " tell currently waiting for you.");
+		try {
+			PreparedStatement checkTells = Database.getPreparedStatement("getTells");
+			checkTells.setString(1, event.getUser().getNick().toLowerCase());
+			ResultSet results = checkTells.executeQuery();
+			while (results.next()) {
+				numTells++;
+			}
+			if (numTells > 0) {
+				event.getUser().send().notice("You have " + numTells + " " + Noun.pluralOf("tell", numTells) + " currently waiting for you.");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-	    }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 	}
 
 	@Override
@@ -68,77 +65,77 @@ public class Tell extends AbstractListener {
 
 	@Override
 	public void handleCommand(String nick, GenericMessageEvent event, String command, String[] copyOfRange) {
-        String sender = nick;
-        if (command.equals(Config.commandprefix + "tell")) {
+		String sender = nick;
+		if (command.equals(Config.commandprefix + "tell")) {
 			if (event.getClass().getName().equals("org.pircbotx.hooks.events.MessageEvent")) {
 				dest = chan;
 			} else {
 				dest = "query";
 			}
 			String message = "";
-        	try {
-    			for( int i = 0; i < copyOfRange.length; i++)
-    			{
-    				message = message + " " + copyOfRange[i];
-    			}
-        		message = message.trim();
-                PreparedStatement addTell = Database.getPreparedStatement("addTell");
-                if (copyOfRange.length == 0) {
-                	event.getBot().sendIRC().message(dest, sender + ": " + "Who did you want to tell?");
-                    return;
-                }
-                String recipient = copyOfRange[0];
-                if (copyOfRange.length == 1) {
-                	event.getBot().sendIRC().message(dest, sender + ": " + "What did you want to say to " + recipient + "?");
-                    return;
-                }
+			try {
+				for( int i = 0; i < copyOfRange.length; i++)
+				{
+					message = message + " " + copyOfRange[i];
+				}
+				message = message.trim();
+				PreparedStatement addTell = Database.getPreparedStatement("addTell");
+				if (copyOfRange.length == 0) {
+					event.getBot().sendIRC().message(dest, sender + ": " + "Who did you want to tell?");
+					return;
+				}
+				String recipient = copyOfRange[0];
+				if (copyOfRange.length == 1) {
+					event.getBot().sendIRC().message(dest, sender + ": " + "What did you want to say to " + recipient + "?");
+					return;
+				}
 
-                String channel = dest;
-                SimpleDateFormat f = new SimpleDateFormat("MMM dd @ HH:mm");
-                f.setTimeZone(TimeZone.getTimeZone("UTC"));
-                String messageOut = StringUtils.join(copyOfRange," ", 1, copyOfRange.length) + " on " + f.format(new Date()) + " UTC";
-                addTell.setString(1, sender);
-                addTell.setString(2, recipient.toLowerCase());
-                addTell.setString(3, channel);
-                addTell.setString(4, messageOut);
-                addTell.executeUpdate();
-            	event.getBot().sendIRC().message(dest, sender + ": " + recipient + " will be notified of this message when next seen.");
-            } catch (Exception e) {
-                e.printStackTrace();
-            	event.getBot().sendIRC().message(dest, sender + ": " + "An error occurred while processing this command (" + Config.commandprefix + "tell)");
-            }
-        }
+				String channel = dest;
+				SimpleDateFormat f = new SimpleDateFormat("MMM dd @ HH:mm");
+				f.setTimeZone(TimeZone.getTimeZone("UTC"));
+				String messageOut = StringUtils.join(copyOfRange," ", 1, copyOfRange.length) + " on " + f.format(new Date()) + " UTC";
+				addTell.setString(1, sender);
+				addTell.setString(2, recipient.toLowerCase());
+				addTell.setString(3, channel);
+				addTell.setString(4, messageOut);
+				addTell.executeUpdate();
+				event.getBot().sendIRC().message(dest, sender + ": " + recipient + " will be notified of this message when next seen.");
+			} catch (Exception e) {
+				e.printStackTrace();
+				event.getBot().sendIRC().message(dest, sender + ": " + "An error occurred while processing this command (" + Config.commandprefix + "tell)");
+			}
+		}
 
 	}
 
 	@Override
-    public void handleMessage(String sender, MessageEvent event,  String[] args) {
-        try {
-        	String nick = "";
+	public void handleMessage(String sender, MessageEvent event,  String[] args) {
+		try {
+			String nick = "";
 			if (nick.contains("@")) {
 				nick = "@" + sender;
-	            PreparedStatement checkTells = Database.getPreparedStatement("getTells");
-	            checkTells.setString(1, nick.toLowerCase());
-	            ResultSet results = checkTells.executeQuery();
-	            while (results.next()) {
-	            	Helper.sendMessage("Corded", nick.replace("@", "") +": " + results.getString(2) + " in " + results.getString(3) + " said: " + results.getString(4));
-	            }
-	            PreparedStatement clearTells = Database.getPreparedStatement("removeTells");
-	            clearTells.setString(1, nick.toLowerCase());
-	            clearTells.execute();
+				PreparedStatement checkTells = Database.getPreparedStatement("getTells");
+				checkTells.setString(1, nick.toLowerCase());
+				ResultSet results = checkTells.executeQuery();
+				while (results.next()) {
+					Helper.sendMessage("Corded", nick.replace("@", "") +": " + results.getString(2) + " in " + results.getString(3) + " said: " + results.getString(4));
+				}
+				PreparedStatement clearTells = Database.getPreparedStatement("removeTells");
+				clearTells.setString(1, nick.toLowerCase());
+				clearTells.execute();
 			} else {
-	            PreparedStatement checkTells = Database.getPreparedStatement("getTells");
-	            checkTells.setString(1, sender.toLowerCase());
-	            ResultSet results = checkTells.executeQuery();
-	            while (results.next()) {
-	                event.getBot().sendIRC().notice(sender, results.getString(2) + " in " + results.getString(3) + " said: " + results.getString(4));
-	            }
-	            PreparedStatement clearTells = Database.getPreparedStatement("removeTells");
-	            clearTells.setString(1, sender.toLowerCase());
-	            clearTells.execute();
+				PreparedStatement checkTells = Database.getPreparedStatement("getTells");
+				checkTells.setString(1, sender.toLowerCase());
+				ResultSet results = checkTells.executeQuery();
+				while (results.next()) {
+					event.getBot().sendIRC().notice(sender, results.getString(2) + " in " + results.getString(3) + " said: " + results.getString(4));
+				}
+				PreparedStatement clearTells = Database.getPreparedStatement("removeTells");
+				clearTells.setString(1, sender.toLowerCase());
+				clearTells.execute();
 			}
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
