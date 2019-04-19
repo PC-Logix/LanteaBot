74ce1d377c1e42cd4a813c785aa3481c8a81fcba
Forecaster <martinek90@hotmail.com>
2019-04-19 11:54:11 +0200

    Um.... typo

--- src/main/java/pcl/lc/irc/hooks/DrinkPotion.java
+++ src/main/java/pcl/lc/irc/hooks/DrinkPotion.java
@@ -208,7 +208,7 @@ public class DrinkPotion extends AbstractListener {
 		effects.add("{user} gains the ability to talk to {transformations}.");
 		effects.add("You see the sky briefly flash solid dark blue then go back to normal.");
 		effects.add("When you drink the last drop, a bucket of water materializes above your head and dumps it contents over you, then vanishes. The water does not.");
-		effects.add("Suddenly there's a swarm of wasps is behind you!");
+		effects.add("Suddenly there's a swarm of wasps behind you!");
 		effects.add("When you bring the bottle down you see a {color} colored plastic flamingo. It stares into your soul.");
 		effects.add("A bard starts playing a lute behind you. They don't stop.");
 		effects.add("The bottle turns into a sword.");
