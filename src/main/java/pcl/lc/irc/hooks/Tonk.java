d1523bd4ec9b298c46c6d813c744dc5788d68320
Forecaster <martinek90@hotmail.com>
2019-08-05 15:50:45 +0200

    Make me pretty again

--- src/main/java/pcl/lc/irc/hooks/Tonk.java
+++ src/main/java/pcl/lc/irc/hooks/Tonk.java
@@ -299,19 +299,17 @@ public class Tonk extends AbstractListener {
 				e1.printStackTrace();
 			}
 
-			response += "<div>" +
-					"<ul>" +
-					"<li>Tonk is a game about timing, patience and opportunity, mixed with an idle game.</li>" +
-					"<li>To tonk successfully you must have waited past the current record. Trying to Tonk too early resets the timer.</li>" +
-					"<li>Tonking successfully gives you points depending on the difference between the old record and your new record, unless you held the previous record.</li>" +
-					"<li>Instead of Tonking you can Tonkout, this works the same as Tonk, except after claiming the new record, the entire record is converted to points.</li>" +
-					"<li>If the number of hours in the record is over 2 hours you get bonus points, with a multiplier based on the number of hours in the record minus 1.</li>" +
-					"<li>If you held the previous record you get 100% of the multiplied bonus, if not you get 50%.</li>" +
-					"</ul>" +
-					"</div>";
-			
-			
-			String tonkLeaders = "<table>";
+            String tonkLeaders = "<div>" +
+                    "<ul>" +
+                    "<li>Tonk is a game about timing, patience and opportunity, mixed with an idle game.</li>" +
+                    "<li>To tonk successfully you must have waited past the current record. Trying to Tonk too early resets the timer.</li>" +
+                    "<li>Tonking successfully gives you points depending on the difference between the old record and your new record, unless you held the previous record.</li>" +
+                    "<li>Instead of Tonking you can Tonkout, this works the same as Tonk, except after claiming the new record, the entire record is converted to points.</li>" +
+                    "<li>If the number of hours in the record is over 2 hours you get bonus points, with a multiplier based on the number of hours in the record minus 1.</li>" +
+                    "<li>If you held the previous record you get 100% of the multiplied bonus, if not you get 50%.</li>" +
+                    "</ul>" +
+                    "</div>" +
+                    "<table>";
 			try {
 				PreparedStatement statement = Database.getPreparedStatement("getTonkUsers");
 				ResultSet resultSet = statement.executeQuery();
