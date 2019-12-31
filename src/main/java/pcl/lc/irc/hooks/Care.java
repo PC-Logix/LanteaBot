c7de7ba5b1553854a1897c1a86286ffb481c8206
Forecaster <martinek90@hotmail.com>
2019-12-31 21:14:12 +0100

    Add care command

---
+++ src/main/java/pcl/lc/irc/hooks/Care.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package pcl.lc.irc.hooks;
+
+import org.pircbotx.Colors;
+import org.pircbotx.hooks.events.MessageEvent;
+import org.pircbotx.hooks.types.GenericMessageEvent;
+import pcl.lc.irc.AbstractListener;
+import pcl.lc.irc.Command;
+import pcl.lc.irc.IRCBot;
+import pcl.lc.utils.Helper;
+import pcl.lc.utils.Item;
+import pcl.lc.utils.ItemCollection;
+
+import java.util.ArrayList;
+
+/**
+ * @author Caitlyn
+ *
+ */
+@SuppressWarnings("rawtypes")
+public class Care extends AbstractListener {
+	private Command local_command;
+	private double base_drop_chance = .20; // 0.08 is added for every item included
+
+	@Override
+	protected void initHook() {
+		local_command = new Command("care", 60) {
+			@Override
+			public void onExecuteSuccess(Command command, String nick, String target, GenericMessageEvent event, ArrayList<String> params) {
+				Helper.sendMessage(target, Helper.get_care_response(), nick);
+			}
+		};
+		IRCBot.registerCommand(local_command, "Measure care levels");
+		local_command.registerAlias("care-o-meter");
+		local_command.registerAlias("careometer");
+		local_command.registerAlias("doicare");
+		local_command.registerAlias("howmuchcare");
+	}
+
+	public String chan;
+	public String target = null;
+	@Override
+	public void handleCommand(String sender, MessageEvent event, String command, String[] args, String callingRelay) {
+		chan = event.getChannel().getName();
+	}
+
+	@Override
+	public void handleCommand(String nick, GenericMessageEvent event, String command, String[] copyOfRange, String callingRelay) {
+		target = Helper.getTarget(event);
+		local_command.tryExecute(command, nick, target, event, copyOfRange);
+	}
+}
