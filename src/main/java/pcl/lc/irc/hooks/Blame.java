4b4f87890fecc12e6c72788defac395a0b5ab861
Forecaster <martinek90@hotmail.com>
2019-12-31 21:30:00 +0100

    Add more blame strings

--- src/main/java/pcl/lc/irc/hooks/Blame.java
+++ src/main/java/pcl/lc/irc/hooks/Blame.java
@@ -41,7 +41,7 @@ public class Blame extends AbstractListener {
 		events.add("the existence of wasps!");
 		events.add("E.T for Atari being terrible!");
 		events.add("space being cold!");
-		events.add("all of the bugs%&");
+		events.add("all NaN bugs");
 		events.add("running being exhausting");
 		events.add("ruptured tires");
 		events.add("the existence of trolls");
@@ -52,6 +52,26 @@ public class Blame extends AbstractListener {
 		events.add("Half-life 3 not being out");
 		events.add("doubling the time until release by asking questions");
 		events.add("the moon not being made of cheese");
+		events.add("leaking the secrets");
+		events.add("even suggesting such things");
+		events.add("letting jerks join the channel");
+		events.add("this text not being very interesting");
+		events.add("the cats taking over in secret");
+		events.add("certain comic book heroes dying and coming back, and dying again");
+		events.add("that one thing. You know");
+		events.add("it not being Friday. Or if it is Friday then for it being Friday");
+		events.add("sticking googly eyes on all of the paintings");
+		events.add("forgetting to feed the tentacle pit");
+		events.add("that one spoon going missing");
+		events.add("no typ so gud");
+		events.add("whatever that was");
+		events.add("the thing that just happened");
+		events.add("the return of Vecna");
+		events.add("1 not equalling 2");
+		events.add("not being fast enough");
+		events.add("doughnuts");
+		events.add("the last nightmare I had");
+		events.add("the next person getting bap'd");
 
 		int roll = Helper.getRandomInt(0, (int)(events.size() * 1.25));
 
