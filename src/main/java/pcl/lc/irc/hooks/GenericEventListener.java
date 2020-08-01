cb2782fb28e8b6d89e0391a33b8c46a87c5c4df9
Forecaster <martinek90@hotmail.com>
2020-08-01 15:42:32 +0200

    Also fix command case sensitivity over bridges

--- src/main/java/pcl/lc/irc/hooks/GenericEventListener.java
+++ src/main/java/pcl/lc/irc/hooks/GenericEventListener.java
@@ -71,7 +71,7 @@ public class GenericEventListener extends AbstractListener{
 							}
 							if (split[0].startsWith(startBracket) && split[0].endsWith(endBracket)) {
 								user = Helper.cleanDiscordNick(split[0].substring(1, split[0].length() - 1));
-								command = split[1];
+								command = split[1].toLowerCase();
 								if (command.startsWith(Config.commandprefix)) {
 									System.out.println("Command received over bridge");
 									actualCommand = command.replaceFirst("\\" + Config.commandprefix, "");
