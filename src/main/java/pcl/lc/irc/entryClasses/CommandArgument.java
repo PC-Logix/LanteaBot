c205bdd249f3543fca0b3fcf4be705b914148159
Forecaster <forecaster@towerofawesome.org>
2021-08-30 13:20:52 +0200

    Add docs to CommandArgument constructors

--- src/main/java/pcl/lc/irc/entryClasses/CommandArgument.java
+++ src/main/java/pcl/lc/irc/entryClasses/CommandArgument.java
@@ -9,18 +9,33 @@ public class CommandArgument {
 	public String arg;
 	public ArrayList<String> argList;
 
+	/**
+	 *
+	 * @param type The argument type, see docs of CommandArgumentParser constructor for list
+	 */
 	public CommandArgument(String type) {
 		this.name = null;
 		this.description = null;
 		this.type = type;
 	}
 
+	/**
+	 *
+	 * @param type The argument type, see docs of CommandArgumentParser constructor for list
+	 * @param name The argument name displayed in command help contexts
+	 */
 	public CommandArgument(String type, String name) {
 		this.name = name;
 		this.description = null;
 		this.type = type;
 	}
 
+	/**
+	 *
+	 * @param type The argument type, see docs of CommandArgumentParser constructor for list
+	 * @param name The argument name displayed in context help strings
+	 * @param description Argument description displayed in command help contexts
+	 */
 	public CommandArgument(String type, String name, String description) {
 		this.name = name;
 		this.description = description;
